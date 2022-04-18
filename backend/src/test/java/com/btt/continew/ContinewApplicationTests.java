0253431d38d048857796333369194dcbe0cedcf3
이승기 <tmdrl0160@naver.com>
2022-04-18 03:14:58 +0000

    Revert "[S06P31D105-60] feat: 깃이그노어 적용"
    This reverts commit dfc397ebe6863181f302c3672fd940cd2aa05165

---
+++ backend/src/test/java/com/btt/continew/ContinewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btt.continew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
