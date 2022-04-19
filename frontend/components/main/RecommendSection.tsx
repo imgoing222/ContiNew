ab3a07a4906ac0c802833a57012d82237acc951d
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-19 13:11:09 +0900

    [S06P31D105-66] feat: RecommendSection UI 구조 생성

--- frontend/components/main/RecommendSection.tsx
+++ frontend/components/main/RecommendSection.tsx
@@ -1,9 +1,50 @@
+import styled from "styled-components";
+
 interface RecommendDataProps {
 	recommendData: { id: number; imageUrl: string }[];
 }
 
 function RecommendSection({ recommendData }: RecommendDataProps) {
-	return <div>추천 매물</div>;
+	return (
+		<Section>
+      <Title>oo동 추천매물</Title>
+			<Ul>
+				{recommendData.map((recommend) => (
+					<Li>
+						<Image src={recommend.imageUrl} alt="recommend-img" />
+					</Li>
+				))}
+			</Ul>
+		</Section>
+	);
 }
 
+const Section = styled.section`
+	position: relative;
+  display: flex;
+  flex-direction: column;
+	justify-content: center;
+	height: 40rem;
+`;
+
+const Title = styled.div`
+  height: 5rem;
+  font-size: 3rem;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+	justify-content: center;
+	list-style: none;
+  padding: 0;
+`;
+
+const Li = styled.li`
+  margin: 1rem;
+`;
+
+const Image = styled.img`
+	width: 15rem;
+	height: 15rem;
+`;
 export default RecommendSection;
