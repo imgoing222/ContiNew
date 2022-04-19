ff448e971b9804c9805ed5e7370e6683ce333ea0
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-19 13:22:08 +0900

    [S06P31D105-12] feat: MemberService 에 findByLoginId 추가

--- backend/src/main/java/com/btt/continew/member/service/MemberService.java
+++ backend/src/main/java/com/btt/continew/member/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.btt.continew.member.service;
 
+import com.btt.continew.global.exception.BusinessException;
+import com.btt.continew.global.exception.ErrorCode;
+import com.btt.continew.member.domain.Member;
 import com.btt.continew.member.domain.MemberRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberService {
@@ -14,5 +18,11 @@ public class MemberService {
         PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional(readOnly = true)
+    public Member findByEmail(String id) {
+        return memberRepository.findByLoginId(id)
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_ID));
     }
 }
