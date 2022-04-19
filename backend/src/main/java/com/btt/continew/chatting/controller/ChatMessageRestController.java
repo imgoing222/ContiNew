8244c1351d270966d4b2c114df8b26e5215b8f67
vtaco <woosj7129@naver.com>
2022-04-19 11:43:51 +0900

    [S06P31D105-24] ChatMessageRestController (임시) Redis Test

--- backend/src/main/java/com/btt/continew/chatting/controller/ChatMessageRestController.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/ChatMessageRestController.java
@@ -1,6 +1,6 @@
 package com.btt.continew.chatting.controller;
 
-
+import com.btt.continew.chatting.service.ChatMessageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/chatting")
 public class ChatMessageRestController {
 
+    private final ChatMessageService chatMessageService;
 
+    public ChatMessageRestController(ChatMessageService chatMessageService){
+        this.chatMessageService = chatMessageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postChatting(){
+
+        chatMessageService.createChatMessage();
+
+        return ResponseEntity.noContent().build();
+    }
 }
