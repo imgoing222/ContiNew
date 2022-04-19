b63c9db064b28096d1d53eb7b929eed8f834fb49
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-19 12:13:06 +0900

    [S06P31D105-66] feat: 추천매물 DUMMY_DATA 생성

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,23 @@
 import { SearchSection, RecommendSection } from "../components/main";
 
+const DUMMY_DATA = [
+	{
+		id: 1,
+		imageUrl:
+			"https://images.unsplash.com/photo-1505691723518-36a5ac3be353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+	},
+	{
+		id: 2,
+		imageUrl:
+			"https://images.unsplash.com/flagged/photo-1573168710465-7f7da9a23a15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+	},
+	{
+		id: 3,
+		imageUrl:
+			"https://images.unsplash.com/photo-1560185127-6ed189bf02f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+	},
+];
+
 function MainPage() {
 	return (
 		<>
