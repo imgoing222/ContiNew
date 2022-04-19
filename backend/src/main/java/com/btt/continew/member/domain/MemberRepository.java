0afdaa5c35951edf2c25305ac88263f71624add7
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-19 13:21:47 +0900

    [S06P31D105-12] feat: MemberRepository 에 findByLoginId 추가

--- backend/src/main/java/com/btt/continew/member/domain/MemberRepository.java
+++ backend/src/main/java/com/btt/continew/member/domain/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.btt.continew.member.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByLoginId(String loginId);
 }
