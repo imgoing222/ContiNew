8dd09ae0977dda385e9b4b9130a4ea2a418b7cdd
yeonju501 <loling970124@gmail.com>
2022-04-19 21:29:00 +0900

    [S06P31D105-48] feat: HouseService repository 주입

--- backend/src/main/java/com/btt/continew/house/service/HouseService.java
+++ backend/src/main/java/com/btt/continew/house/service/HouseService.java
@@ -1,8 +1,24 @@
 package com.btt.continew.house.service;
 
+import com.btt.continew.house.controller.dto.request.HouseSaveRequest;
+import com.btt.continew.house.domain.HouseRepository;
+import com.btt.continew.member.domain.Member;
+import com.btt.continew.member.service.MemberService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HouseService {
 
+    private final HouseRepository houseRepository;
+    private final MemberService memberService;
+
+    public HouseService(HouseRepository houseRepository, MemberService memberService) {
+        this.houseRepository = houseRepository;
+        this.memberService = memberService;
+    }
+
+    public void create(HouseSaveRequest request, String email) {
+        Member member = memberService.findByEmail(email);
+
+    }
 }
