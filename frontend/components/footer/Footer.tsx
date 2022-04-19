8c751d6950690c7842f80b59d3579afb429c8b92
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-19 14:29:18 +0900

    [S06P31D105-66] style: Footer styled-components 추가

--- frontend/components/footer/Footer.tsx
+++ frontend/components/footer/Footer.tsx
@@ -1,13 +1,36 @@
+import styled from "styled-components";
+
 function Footer() {
 	return (
-		<footer>
-			<p>(주)SSAFY6</p>
-			<p>상호: ContiNew</p>
-			<p>대표: 우팀장</p>
-			<p>사업자 번호: 123-45-678910</p>
-			<p>주소: 서울 강남구 밤고개로 99</p>
-		</footer>
+		<Container>
+			<SubContainer>
+				<Content>(주)SSAFY6</Content>
+				<Content>상호: ContiNew</Content>
+				<Content>대표: 우팀장</Content>
+				<Content>사업자 번호: 123-45-678910</Content>
+				<Content>주소: 서울 강남구 밤고개로 99</Content>
+			</SubContainer>
+		</Container>
 	);
 }
+
+const Container = styled.footer`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background: #696969;
+`;
+
+const SubContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+  margin: 1rem
+`;
+
+const Content = styled.p`
+	font-size: 1rem;
+	color: #d3d3d3;
+	margin: 0;
+`;
 
 export default Footer;
