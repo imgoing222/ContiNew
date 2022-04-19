00b7abcd21d231791f975e8a3271410ef3455473
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-19 14:20:13 +0900

    [S06P31D105-66] feat: Footer 파일 생성

---
+++ frontend/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+	return (
+		<footer>
+			<p>(주)SSAFY6</p>
+			<p>상호: ContiNew</p>
+			<p>대표: 우팀장</p>
+			<p>사업자 번호: 123-45-678910</p>
+			<p>주소: 서울 강남구 밤고개로 99</p>
+		</footer>
+	);
+}
+
+export default Footer;
