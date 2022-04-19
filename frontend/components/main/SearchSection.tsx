41811aff7f7b36539c877e2a5a1109934f0044c8
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-19 12:01:43 +0900

    [S06P31D105-66] feat: background image 추가

--- frontend/components/main/SearchSection.tsx
+++ frontend/components/main/SearchSection.tsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 function SearchSection() {
-  return <div>Img</div>
+	return <Section></Section>;
 }
+
+const Section = styled.section`
+	position: relative;
+	height: 30rem;
+	background: url("/main_background.jpg");
+`;
 
 export default SearchSection;
