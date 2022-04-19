75426b31faaf40caebc93646f4f5fc075e440818
vtaco <woosj7129@naver.com>
2022-04-19 11:42:12 +0900

    [S06P31D105-24] ChatMessageRepository 생성

---
+++ backend/src/main/java/com/btt/continew/chatting/domain/ChatMessageRepository.java
@@ -0,0 +1,7 @@
+package com.btt.continew.chatting.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChatMessageRepository extends CrudRepository<ChatMessage, String> {
+
+}
