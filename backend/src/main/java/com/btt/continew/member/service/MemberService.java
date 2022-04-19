afd06c803ba9d5e2e4f4f8245691133311944c10
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-19 13:51:40 +0900

    [S06P31D105-12] feat: MemberService 에 회원가입 관련 추가

--- backend/src/main/java/com/btt/continew/member/service/MemberService.java
+++ backend/src/main/java/com/btt/continew/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.btt.continew.member.service;
 
 import com.btt.continew.global.exception.BusinessException;
 import com.btt.continew.global.exception.ErrorCode;
+import com.btt.continew.member.controller.dto.request.MemberSaveRequest;
 import com.btt.continew.member.domain.Member;
 import com.btt.continew.member.domain.MemberRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +24,28 @@ public class MemberService {
     @Transactional(readOnly = true)
     public Member findByEmail(String id) {
         return memberRepository.findByLoginId(id)
-            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_ID));
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_LOGIN_ID));
+    }
+
+    @Transactional
+    public Long saveMember(MemberSaveRequest request) {
+        checkDuplicateLoginId(request.getLoginId());
+        checkDuplicateUsername(request.getUsername());
+
+        Member member = request.toMember();
+        member.encodePassword(passwordEncoder);
+        return memberRepository.save(member).getId();
+    }
+
+    public void checkDuplicateLoginId(String loginId) {
+        if (memberRepository.existsByLoginId(loginId)) {
+            throw new BusinessException(ErrorCode.MEMBER_LOGIN_ID_DUPLICATED);
+        }
+    }
+
+    public void checkDuplicateUsername(String username) {
+        if (memberRepository.existsByUsername(username)) {
+            throw new BusinessException(ErrorCode.MEMBER_USERNAME_DUPLICATED);
+        }
     }
 }
