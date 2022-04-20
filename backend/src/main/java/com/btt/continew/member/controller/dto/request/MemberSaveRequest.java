2de38b27ce097e2ad84a918b3212f2a5269e97e5
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-20 11:47:17 +0900

    [S06P31D105-17] feat: MemberSaveRequest 에 Swagger 요소 추가

--- backend/src/main/java/com/btt/continew/member/controller/dto/request/MemberSaveRequest.java
+++ backend/src/main/java/com/btt/continew/member/controller/dto/request/MemberSaveRequest.java
@@ -8,15 +8,15 @@ import lombok.Getter;
 @Getter
 public class MemberSaveRequest {
 
-    @ApiModelProperty(name = "login_id", position = 0, example = "continew@naver.com")
+    @ApiModelProperty(name = "login_id", position = 0, notes = "로그인 아이디", example = "continew")
     @JsonProperty("login_id")
     private String loginId;
 
-    @ApiModelProperty(name = "password", position = 1, example = "Zjsxlsb123!!")
+    @ApiModelProperty(name = "password", position = 1, notes = "비밀번호", example = "Zjsxlsb123!!")
     @JsonProperty("password")
     private String password;
 
-    @ApiModelProperty(name = "username", position = 2, example = "컨티뉴")
+    @ApiModelProperty(name = "username", position = 2, notes = "닉네임", example = "컨티뉴")
     @JsonProperty("username")
     private String username;
 
