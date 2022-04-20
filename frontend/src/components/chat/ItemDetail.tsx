e114bedef58e54414ec35941f51cbc1e59be29f7
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-20 14:59:28 +0900

    [S06P31D105-42] feat: Detail UI 초안 추가

--- frontend/src/components/chat/ItemDetail.tsx
+++ frontend/src/components/chat/ItemDetail.tsx
@@ -1,5 +1,21 @@
 function ItemDetail() {
-  return <></>
-};
+	return (
+		<>
+			<div>
+				<h3>Detail</h3>
+			</div>
+      <div>
+        <img src="" alt="Img" />
+      </div>
+      <div>
+        <h3>가격정보</h3>
+        <p>월세: 2000/30</p>
+        <p>관리비: 매월 5만 원</p>
+        <h3>위치</h3>
+        <p>서울특별시 동작구 사당동</p>
+      </div>
+		</>
+	);
+}
 
 export default ItemDetail;
