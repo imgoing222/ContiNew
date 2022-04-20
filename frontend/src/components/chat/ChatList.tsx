b1b464f057a53a14e61c6dd1247f55dbdc57c638
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-20 14:07:56 +0900

    [S06P31D105-40] style: ChatList styled-components 추가

--- frontend/src/components/chat/ChatList.tsx
+++ frontend/src/components/chat/ChatList.tsx
@@ -1,6 +1,8 @@
+import styled from "styled-components";
+
 function ChatList() {
 	return (
-		<>
+		<Container>
 			<div>
 				<h3>Messages</h3>
 			</div>
@@ -22,8 +24,16 @@ function ChatList() {
 					<p>최근메시지</p>
 				</div>
 			</div>
-		</>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+  width: 300px;
+	border-right: solid 2px #d3d3d3;
+`;
 
 export default ChatList;
