27d3ed5d781295f72cd7e2db867501d3d6a4f4fa
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-20 14:12:15 +0900

    [S06P31D105-40] style: ChatPage styled-components 추가

--- frontend/src/pages/chat/index.tsx
+++ frontend/src/pages/chat/index.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+import ChatList from "@components/chat/ChatList";
+
 function ChatPage() {
-  return <></>
-};
+	return (
+		<Container>
+			<ChatList />
+		</Container>
+	);
+}
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	border: solid 2px #d3d3d3;
+`;
 
 export default ChatPage;
