7dce110c1d31b965dceffc5cf60369a51a8f3c45
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-20 13:24:58 +0900

    [S06P31D105-21] feat: AuthService 에 로그인시 refresh_token 도 반환

--- backend/src/main/java/com/btt/continew/auth/service/AuthService.java
+++ backend/src/main/java/com/btt/continew/auth/service/AuthService.java
@@ -2,6 +2,9 @@ package com.btt.continew.auth.service;
 
 import com.btt.continew.auth.controller.dto.request.LoginRequest;
 import com.btt.continew.auth.controller.dto.response.TokenResponse;
+import com.btt.continew.auth.domain.Authority;
+import com.btt.continew.auth.domain.RefreshToken;
+import com.btt.continew.auth.domain.RefreshTokenRepository;
 import com.btt.continew.auth.infrastructure.JwtTokenProvider;
 import com.btt.continew.member.domain.Member;
 import com.btt.continew.member.service.MemberService;
@@ -17,12 +20,14 @@ public class AuthService {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public AuthService(MemberService memberService, PasswordEncoder passwordEncoder,
-        JwtTokenProvider jwtTokenProvider) {
+        JwtTokenProvider jwtTokenProvider, RefreshTokenRepository refreshTokenRepository) {
         this.memberService = memberService;
         this.passwordEncoder = passwordEncoder;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     @Transactional
@@ -31,16 +36,35 @@ public class AuthService {
         member.CheckPasswordForLogin(passwordEncoder, request.getPassword());
 
         TokenResponse tokenResponse = jwtTokenProvider.createToken(member.getLoginId(), member.getAuthority());
-
-        setTokenToCookie(tokenResponse.getAccessToken(), response);
+        String refreshToken = saveRefreshToken(member, tokenResponse);
+        setTokenToCookie(tokenResponse.getAccessToken(), refreshToken, response);
     }
 
-    private void setTokenToCookie(String accessToken, HttpServletResponse response) {
+    @Transactional
+    public String saveRefreshToken(Member member, TokenResponse tokenResponse) {
+        RefreshToken refreshToken = refreshTokenRepository.findBySubject(member.getLoginId())
+            .orElse(RefreshToken.builder()
+                .subject(member.getLoginId())
+                .refreshToken(tokenResponse.getRefreshToken())
+                .authority(Authority.ROLE_MEMBER)
+                .build());
+        refreshTokenRepository.save(refreshToken);
+        return refreshToken.getRefreshToken();
+    }
+
+    private void setTokenToCookie(String accessToken, String refreshToken, HttpServletResponse response) {
         Cookie accessTokenCookie = new Cookie("access_token", accessToken);
         accessTokenCookie.setMaxAge(30 * 60); // expires in 30 minutes
         accessTokenCookie.setSecure(true);
 //        accessTokenCookie.setHttpOnly(true);
         accessTokenCookie.setPath("/");
         response.addCookie(accessTokenCookie);
+
+        Cookie refreshTokenCookie = new Cookie("refresh_token", refreshToken);
+        refreshTokenCookie.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
+        refreshTokenCookie.setSecure(true);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setPath("/");
+        response.addCookie(refreshTokenCookie);
     }
 }
