30826942cdc5fc1baa811748ca69560d9e39c135
vtaco <woosj7129@naver.com>
2022-04-20 16:24:34 +0900

    [S06P31D105-23] SocketHandler 메시지 전송

--- backend/src/main/java/com/btt/continew/chatting/handler/SocketHandler.java
+++ backend/src/main/java/com/btt/continew/chatting/handler/SocketHandler.java
@@ -1,27 +1,20 @@
 package com.btt.continew.chatting.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Slf4j
 @Component
 public class SocketHandler extends TextWebSocketHandler {
-    
-    @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message){
-        // 메시지 발송
-    }
-    
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception{
-        // 소켓 연결
-    }
-    
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        // 소켓 종료
-    }
 
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
+        String payload = message.getPayload();
+        log.info("payload = {}", payload);
+        TextMessage textMessage = new TextMessage("Welcome");
+        session.sendMessage(textMessage);
+    }
 }
