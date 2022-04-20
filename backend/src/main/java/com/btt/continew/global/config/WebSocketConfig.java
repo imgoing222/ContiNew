a750d83cef8b27f6f6db7be5ea79fb3f197f4f2f
vtaco <woosj7129@naver.com>
2022-04-20 16:24:12 +0900

    [S06P31D105-23] WebSocketConfig socket 등록

--- backend/src/main/java/com/btt/continew/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/btt/continew/global/config/WebSocketConfig.java
@@ -1,5 +1,21 @@
 package com.btt.continew.global.config;
 
-public class WebSocketConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+        registry.addHandler(webSocketHandler,"/ws/chat").setAllowedOrigins("*");
+    }
 }
