149af484e85c28b9fa568e8d1011c475620790a2
yeonju501 <loling970124@gmail.com>
2022-04-20 16:56:02 +0900

    [S06P31D105-72] feat: ImageUploader convert 추가

--- backend/src/main/java/com/btt/continew/image/ImageUploader.java
+++ backend/src/main/java/com/btt/continew/image/ImageUploader.java
@@ -47,4 +47,15 @@ public class ImageUploader {
             .withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
 }
