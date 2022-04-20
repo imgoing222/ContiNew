0ca128ab8b168df7885eb3aa9e3ec3d7e457ae3a
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-20 13:57:13 +0900

    [S06P31D105-40] feat: ChatList UI 임시초안 추가

--- frontend/src/components/chat/ChatList.tsx
+++ frontend/src/components/chat/ChatList.tsx
@@ -1,5 +1,29 @@
 function ChatList() {
-  return <></>
-};
+	return (
+		<>
+			<div>
+				<h3>Messages</h3>
+			</div>
+			<div>
+				<div>
+					<p>닉네임</p>
+					<p>최근메시지</p>
+				</div>
+				<div>
+					<p>닉네임</p>
+					<p>최근메시지</p>
+				</div>
+				<div>
+					<p>닉네임</p>
+					<p>최근메시지</p>
+				</div>
+				<div>
+					<p>닉네임</p>
+					<p>최근메시지</p>
+				</div>
+			</div>
+		</>
+	);
+}
 
 export default ChatList;
