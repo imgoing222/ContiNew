b2988701185a74e2db346c4e45e75ba74e8ebfdb
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-20 14:26:28 +0900

    [S06P31D105-40] feat: ChatList DUMMY_DATA 생성

--- frontend/src/pages/chat/index.tsx
+++ frontend/src/pages/chat/index.tsx
@@ -2,6 +2,30 @@ import styled from "styled-components";
 
 import ChatList from "@components/chat/ChatList";
 
+const DUMMY_DATA = [
+	{
+		chatId: 1,
+		sellerNickname: "김싸피",
+		buyerNickname: "이싸피",
+		lastContent: "화장실 사진 좀 찍어주세요.",
+		updatedAt: "2022-04-20 14:22:04"
+	},
+	{
+		chatId: 2,
+		sellerNickname: "박싸피",
+		buyerNickname: "유싸피",
+		lastContent: "화장실 사진 좀 찍어주세요.",
+		updatedAt: "2022-04-19 18:58:22"
+	},
+	{
+		chatId: 3,
+		sellerNickname: "조싸피",
+		buyerNickname: "이싸피",
+		lastContent: "화장실 사진 좀 찍어주세요.",
+		updatedAt: "2022-04-19 08:02:39"
+	}
+];
+
 function ChatPage() {
 	return (
 		<Container>
