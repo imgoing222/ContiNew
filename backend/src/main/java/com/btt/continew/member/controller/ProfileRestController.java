e742df1ac22a70748333c330f85deb9657d4b9e2
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-22 11:44:26 +0900

    [S06P31D105-13] feat: ProfileRestController 에 changeMemberInfo 추가

--- backend/src/main/java/com/btt/continew/member/controller/ProfileRestController.java
+++ backend/src/main/java/com/btt/continew/member/controller/ProfileRestController.java
@@ -1,5 +1,6 @@
 package com.btt.continew.member.controller;
 
+import com.btt.continew.member.controller.dto.request.MemberChangeRequest;
 import com.btt.continew.member.controller.dto.response.MemberInfoResponse;
 import com.btt.continew.member.service.ProfileService;
 import io.swagger.annotations.Api;
@@ -8,6 +9,8 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +26,17 @@ public class ProfileRestController {
     }
 
     @GetMapping("/auth/members")
-    @ApiOperation(value = "회원 정보 조회", notes = "회원 정보 조회 api")
+    @ApiOperation(value = "회원 정보 조회", notes = "<span style=\"font-weight=550; color=#ff5f5f\">(로그인 필요)</span> 회원 정보 조회")
     public ResponseEntity<MemberInfoResponse> showMemberInfo(@ApiParam(hidden = true) @AuthenticationPrincipal String loginId) {
         return ResponseEntity.ok().body(profileService.showMemberInfo(loginId));
+    }
+
+    @PutMapping("/auth/members/info")
+    @ApiOperation(value = "회원정보 변경", notes = "<span style=\"font-weight=550; color=#ff5f5f\">(로그인 필요)</span> 닉네임 변경\n"
+        + "현재는 닉네임밖에 없으나 추후 더 생길것을 생각해서 api 주소를 info로 하였음")
+    public ResponseEntity<Void> changeMemberInfo(@ApiParam(hidden = true) @AuthenticationPrincipal String loginId,
+        @RequestBody MemberChangeRequest request) {
+        profileService.changeMemberInfo(loginId, request);
+        return ResponseEntity.noContent().build();
     }
 }
