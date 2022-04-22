510abc21f846efd754c314915cbaf9863013f4fc
yeonju501 <loling970124@gmail.com>
2022-04-22 16:34:14 +0900

    [S06P31D105-77] feat: HouseRestController 스웨거 설명 추가

--- backend/src/main/java/com/btt/continew/house/controller/HouseRestController.java
+++ backend/src/main/java/com/btt/continew/house/controller/HouseRestController.java
@@ -28,6 +28,23 @@ public class HouseRestController {
 
     @PostMapping("/houses")
     @ApiOperation(value = "매물 등록", notes = "매물 등록 api")
+    @ApiImplicitParam(name = "house", value = "{\n"
+        + "  \"sido_name\": \"경기도\",\n"
+        + "  \"gunguName\": \"광명시\",\n"
+        + "  \"dongName\": \"소하동 1363\",\n"
+        + "  \"roadName\": \"신촌로 49\",\n"
+        + "  \"address_detail\": \"108동\",\n"
+        + "  \"floor\": 5,\n"
+        + "  \"trade_type\": \"이어살기\",\n"
+        + "  \"house_type\": \"원룸\",\n"
+        + "  \"deposit\": 10000000,\n"
+        + "  \"monthly_rent\": 500000,\n"
+        + "  \"maintenance_fee\": 50000,\n"
+        + "  \"maintenance_detail\": \"전기요금\",\n"
+        + "  \"period\": 6,\n"
+        + "  \"description\": \"교환학생 가게 되어서 6개월 살고 급하게 내놓습니다 위치 좋고 남향에다가 어쩌고저쩌고\",\n"
+        + "  \"options\": [1, 2, 3]\n"
+        + "}")
     public ResponseEntity<Void> create(@RequestPart(value = "house") HouseSaveRequest request,
         @RequestPart(value = "images") List<MultipartFile> images,
         @ApiParam(hidden = true) @AuthenticationPrincipal String email) {
