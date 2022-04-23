f08d4c4af0903a96922fbe105098a97a40e20141
우동진 <wjinh93@gmail.com>
2022-04-23 18:56:23 +0900

    [S06P31D105-78] feat: script import

--- frontend/src/components/map/search.tsx
+++ frontend/src/components/map/search.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Script from "next/script";
 
 function inputsearch() {
-	return <div>inputsearch</div>;
+	return (
+		<>
+			<Script
+				type="text/javascript"
+				src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=hbx0fr2s1r&submodules=geocoder"
+			></Script>
+		</>
+	);
 }
 
 export default inputsearch;
