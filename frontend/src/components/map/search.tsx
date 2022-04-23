3ce73beb2514be8286e295181585b9472e88e149
우동진 <wjinh93@gmail.com>
2022-04-23 18:56:44 +0900

    [S06P31D105-78] feat: 검색 UI 구현

--- frontend/src/components/map/search.tsx
+++ frontend/src/components/map/search.tsx
@@ -8,6 +8,12 @@ function inputsearch() {
 				type="text/javascript"
 				src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=hbx0fr2s1r&submodules=geocoder"
 			></Script>
+			<div>
+				<label htmlFor="addressSearch">검색</label>
+				<form onSubmit={handleSubmit}>
+					<input type="text" id="addressSearch" value={search} onChange={handleChange} />
+				</form>
+			</div>
 		</>
 	);
 }
