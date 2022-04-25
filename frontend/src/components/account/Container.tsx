b2115808cd32eee6168a5efe1cf937d5c04004ad
9603100 <imgoing222@gmail.com>
2022-04-26 02:43:46 +0900

    [S06P31D105-74] style: 인증 부분 임시 디자인

--- frontend/src/components/account/Container.tsx
+++ frontend/src/components/account/Container.tsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	width: 60%;
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+`;
 
-export const FormContainer = styled.form``;
+export const FormContainer = styled.form`
+	display: flex;
+	flex-direction: column;
+`;
