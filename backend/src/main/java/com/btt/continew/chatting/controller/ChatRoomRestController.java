fe620f3ac6cd40255e11d37acb2c81390bb0babc
vtaco <woosj7129@naver.com>
2022-04-25 14:39:30 +0900

    [S06P31D105-26] ChatRoomRestController 생성 및 구현

---
+++ backend/src/main/java/com/btt/continew/chatting/controller/ChatRoomRestController.java
@@ -0,0 +1,41 @@
+package com.btt.continew.chatting.controller;
+
+import com.btt.continew.chatting.domain.ChatRoom;
+import com.btt.continew.chatting.domain.ChatRoomRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/api/chatting")
+public class ChatRoomRestController {
+
+    private ChatRoomRepository chatRoomRepository;
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatRoomRepository.findAllRoom();
+    }
+
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomRepository.createChatRoom(name);
+    }
+
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatRoomRepository.findRoomById(roomId);
+    }
+}
