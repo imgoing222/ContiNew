dab8443e21361a296a28e6e6887a34298779c70c
9603100 <imgoing222@gmail.com>
2022-04-25 14:39:13 +0900

    [S06P31D105-74] feat: 회원가입 테스트

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -6,10 +6,14 @@ import { Label } from "@components/account/Label";
 import { LinkButton } from "@components/account/LinkButton";
 
 function Signup() {
+	const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+
 	return (
 		<Container>
 			<Header>회원가입</Header>
-			<FormContainer>
+			<FormContainer onSubmit={handleFormSubmit}>
 				<Label>아이디</Label>
 				<Input placeholder="" />
 				<Label>닉네임</Label>
