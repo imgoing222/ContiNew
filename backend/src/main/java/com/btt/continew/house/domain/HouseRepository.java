005ddfa106bee47b8ad1ac4524386457d3a554a2
yeonju501 <loling970124@gmail.com>
2022-04-25 22:18:08 +0900

    [S06P31D105-77] feat: HouseRepository findAllByLatitudeBetweenAndLongitudeBetween 추가

--- backend/src/main/java/com/btt/continew/house/domain/HouseRepository.java
+++ backend/src/main/java/com/btt/continew/house/domain/HouseRepository.java
@@ -1,7 +1,9 @@
 package com.btt.continew.house.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HouseRepository extends JpaRepository<House, Long> {
-
+    Page<House> findAllByLatitudeBetweenAndLongitudeBetween(Long latStart, Long latEnd, Long lngStart, Long lngEnd, Pageable pageable);
 }
