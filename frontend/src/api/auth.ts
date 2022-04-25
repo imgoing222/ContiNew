d36d6063627e152ec720227ffc7f86a936085596
9603100 <imgoing222@gmail.com>
2022-04-25 22:29:54 +0900

    [S06P31D105-74] feat: 로그인 요청 함수 생성

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -7,10 +7,12 @@ interface AuthApiType {
 		password: string;
 		username: string;
 	}) => Promise<AxiosResponse>;
+	signin: (userInfo: { login_id: string; password: string }) => Promise<AxiosResponse>;
 }
 
 const authApi: AuthApiType = {
 	signup: (userInfo) => request.post("members", userInfo),
+	signin: (userInfo) => request.post("members/login", userInfo),
 };
 
 export default authApi;
