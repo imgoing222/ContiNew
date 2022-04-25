c90147f582ef69c01a87b430c14561498c8d4116
vtaco <woosj7129@naver.com>
2022-04-25 14:39:59 +0900

    [S06P31D105-26] ChatService & SocketHandler - WebSocket 임시 주석처리

--- backend/src/main/java/com/btt/continew/chatting/handler/SocketHandler.java
+++ backend/src/main/java/com/btt/continew/chatting/handler/SocketHandler.java
@@ -1,30 +1,30 @@
-package com.btt.continew.chatting.handler;
-
-import com.btt.continew.chatting.domain.ChatMessage;
-import com.btt.continew.chatting.domain.ChatRoom;
-import com.btt.continew.chatting.service.ChatService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-@Slf4j
-@RequiredArgsConstructor
-@Component
-public class SocketHandler extends TextWebSocketHandler {
-
-    private final ObjectMapper objectMapper;
-    private final ChatService chatService;
-
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
-        String payload = message.getPayload();
-        log.info("payload = {}", payload);
-        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
-        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
-        room.handleActions(session, chatMessage,chatService);
-    }
-}
+//package com.btt.continew.chatting.handler;
+//
+//import com.btt.continew.chatting.domain.ChatMessage;
+//import com.btt.continew.chatting.domain.ChatRoom;
+//import com.btt.continew.chatting.service.ChatService;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@Component
+//public class SocketHandler extends TextWebSocketHandler {
+//
+//    private final ObjectMapper objectMapper;
+//    private final ChatService chatService;
+//
+//    @Override
+//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
+//        String payload = message.getPayload();
+//        log.info("payload = {}", payload);
+//        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+//        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+//        room.handleActions(session, chatMessage,chatService);
+//    }
+//}
