8a29b6c31d4f00d54bfb1f5c1d10be32ec601255
우동진 <wjinh93@gmail.com>
2022-04-25 16:12:50 +0900

    [S06P31D105-33] feat: handleSubmit, handleChange 함수 추가

--- frontend/src/components/SaleList/SearchInput.tsx
+++ frontend/src/components/SaleList/SearchInput.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import { RefProps } from "./SaleListNav";
 
-function SearchInput() {
+function SearchInput({ kakaoMap }: RefProps) {
+	const [keyword, setKeyword] = useState("");
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const geocoder = new window.kakao.maps.services.Geocoder();
+		const places = new window.kakao.maps.services.Places();
+		const searchAddress = (result, status) => {
+			if (result.length === 0) return;
+			if (status === window.kakao.maps.services.Status.OK) {
+				const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+				kakaoMap.current.setCenter(coords);
+			}
+		};
+
+		geocoder.addressSearch(keyword, searchAddress);
+		places.keywordSearch(keyword, searchAddress);
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setKeyword(e.target.value);
+	};
+
 	return (
-		<SearchForm>
-			<Input />
+		<SearchForm onSubmit={handleSubmit}>
+			<Input value={keyword} onChange={handleChange} />
 			<Button>검색</Button>
 		</SearchForm>
 	);
@@ -33,4 +58,7 @@ const Button = styled.button`
 	border: none;
 	font-size: 1.5rem;
 	font-weight: bold;
+	&:hover {
+		cursor: pointer;
+	}
 `;
