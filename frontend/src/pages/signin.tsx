a844eca4ebd244df6b85efc758c84c34c8aa549a
9603100 <imgoing222@gmail.com>
2022-04-25 22:51:18 +0900

    [S06P31D105-74] feat: 로그인 페이지 임시 작성

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,5 +1,42 @@
+import { Button } from "@components/account/Button";
+import { Container, FormContainer } from "@components/account/Container";
+import { Header } from "@components/account/Header";
+import { Input } from "@components/account/Input";
+import { Label } from "@components/account/Label";
+import { LinkButton } from "@components/account/LinkButton";
+import { useState } from "react";
+import authApi from "src/api/auth";
+
 function Signin() {
-	return <h1>로그인</h1>;
+	const [values, setValues] = useState({ login_id: "", password: "" });
+
+	const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(values);
+		const res = await authApi.signin(values);
+		console.log(res);
+		console.log("로그인!");
+	};
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setValues({ ...values, [name]: value.trim() });
+	};
+
+	return (
+		<Container>
+			<Header>로그인</Header>
+			<FormContainer onSubmit={handleFormSubmit}>
+				<Label>아이디</Label>
+				<Input placeholder="" name="login_id" onChange={handleInputChange} />
+				<Label>비밀번호</Label>
+				<Input placeholder="" name="password" type="password" onChange={handleInputChange} />
+				<Button>로그인</Button>
+			</FormContainer>
+			<Button>구글로 시작하기</Button>
+			<LinkButton href="/signin">로그인하러가기</LinkButton>
+		</Container>
+	);
 }
 
 export default Signin;
