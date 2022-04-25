9accccd9fdcd8c309565c4c97f87b08c726c16d0
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-25 15:18:28 +0900

    [S06P31D105-46] feat: 소켓 객체 생성

--- frontend/src/pages/chat/[chatId].tsx
+++ frontend/src/pages/chat/[chatId].tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState, useRef } from "react";
+
 function ChatDetail() {
-  const WebSocket = require("ws");
-  const socket = new WebSocket("ws://localhost:8080/ws/chat");
-  socket.onopen = () => {
-    console.log("connected");
-  }
-  return <></>
-};
+	const webSocketUrl = "ws://localhost:8080/ws/chat";
+	let ws = useRef<WebSocket | null>(null);
+
+	useEffect(() => {
+		if (!ws.current) {
+			ws.current = new WebSocket(webSocketUrl);
+			ws.current.onopen = () => {
+				console.log("connected to " + webSocketUrl);
+			};
+			ws.current.onclose = () => {
+				console.log("disconnect from " + webSocketUrl);
+			};
+			ws.current.onerror = (error) => {
+				console.log("connenction error " + webSocketUrl);
+				console.log(error);
+			};
+			ws.current.onmessage = (evnet) => {
+				const data = JSON.parse(evnet.data);
+				console.log(data);
+			};
+		}
+	});
+	return <></>;
+}
 
 export default ChatDetail;
