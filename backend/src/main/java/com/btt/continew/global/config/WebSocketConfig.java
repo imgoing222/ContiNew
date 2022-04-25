7531ae04c9fd23e7ba372152e57386a149176ff2
vtaco <woosj7129@naver.com>
2022-04-25 14:37:33 +0900

    [S06P31D105-26] WebSocketConfig - stomp 변경

--- backend/src/main/java/com/btt/continew/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/btt/continew/global/config/WebSocketConfig.java
@@ -2,20 +2,36 @@ package com.btt.continew.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WebSocketHandler webSocketHandler;
+//    private final WebSocketHandler webSocketHandler;
+
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+//        registry.addHandler(webSocketHandler,"/ws/chat").setAllowedOrigins("*");
+//    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
-        registry.addHandler(webSocketHandler,"/ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws-stomp/").setAllowedOrigins("*")
+            .withSockJS();
     }
 }
