458ce83cdb5b2392755b582c281a1a9e1ece0327
우동진 <wjinh93@gmail.com>
2022-04-25 14:44:35 +0900

    [S06P31D105-78] feat: style: 클러스터 스타일 적용

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -26,14 +26,29 @@ function Map() {
 
 				// 임시 데터
 				const data = [
-					{ y: 37.3595316, x: 127.1052133, content: "네이버", num: 10 },
-					{ y: 37.3595315, x: 127.1052133, content: "다음", num: 22 },
+					{ y: 37.3595316, x: 127.1052133, content: "네이버" },
+					{ y: 37.359531, x: 127.1052133, content: "다음" },
+					{ y: 37.5559, x: 126.9723, content: "네이버" },
+					{ y: 37.5663, x: 126.9779, content: "네이버" },
+					{ y: 37.5465, x: 126.9647, content: "네이버" },
 				];
 				const clusterer = new window.kakao.maps.MarkerClusterer({
 					map: kakaoMap.current, // 마커들을 클러스터로 관리하고 표시할 지도 객체
 					averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-					minLevel: 8, // 클러스터 할 최소 지도 레벨
+					minLevel: 0, // 클러스터 할 최소 지도 레벨
+					styles: [
+						{
+							background: "rgba(255, 80, 80, .8)",
+							width: "60px",
+							height: "60px",
+							color: "#fff",
+							borderRadius: "30px",
+							textAlign: "center",
+							lineHeight: "61px",
+						},
+					],
 				});
+				clusterer.setMinClusterSize(0);
 				const markers = data.map(
 					(item) =>
 						new window.kakao.maps.Marker({
@@ -58,11 +73,7 @@ function Map() {
 			if (result.length === 0) return;
 			if (status === window.kakao.maps.services.Status.OK) {
 				const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-				// 결과값으로 받은 위치를 마커로 표시
-				const marker = new window.kakao.maps.Marker({
-					map: kakaoMap.current,
-					position: coords,
-				});
+
 				kakaoMap.current.setCenter(coords);
 			}
 		};
