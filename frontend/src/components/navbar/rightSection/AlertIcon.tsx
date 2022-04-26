c4d823ab056bfec3aca9cc77f3cf197235f500cc
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-26 20:32:13 +0900

    [S06P31D105-67] style: 아이콘 크기 수정

--- frontend/src/components/navbar/rightSection/AlertIcon.tsx
+++ frontend/src/components/navbar/rightSection/AlertIcon.tsx
@@ -1,12 +1,19 @@
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 
 function AlertIcon() {
 	return (
 		<>
-			<FontAwesomeIcon icon={faBell} />
+			<FontAwesome icon={faBell} />
 		</>
 	);
 }
+
+const FontAwesome = styled(FontAwesomeIcon)`
+	width: 2rem;
+	height: 2rem;
+	margin-right: 1.5rem;
+`;
 
 export default AlertIcon;
--- frontend/src/components/navbar/rightSection/UserIcon.tsx
+++ frontend/src/components/navbar/rightSection/UserIcon.tsx
@@ -1,12 +1,19 @@
 import { faArrowRightFromBracket, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 
 function UserIcon() {
 	return (
 		<>
-			<FontAwesomeIcon icon={faUser} />
+			<FontAwesome icon={faUser} />
 		</>
 	);
 }
+
+const FontAwesome = styled(FontAwesomeIcon)`
+	width: 2rem;
+	height: 2rem;
+	margin-right: 1.5rem;
+`;
 
 export default UserIcon;
