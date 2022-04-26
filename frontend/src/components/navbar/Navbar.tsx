b782dac8bbf12943eb81f15e5ab952089216e719
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-26 16:03:38 +0900

    [S06P31D105-67] feat: Section 추가

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -3,20 +3,30 @@ import styled from "styled-components";
 function Navbar() {
 	return (
 		<Nav>
-			<div>지도</div>
-			<div>방내놓기</div>
-			<div>채팅</div>
-			<div>프로필</div>
-			<div>로그아웃</div>
+			<Section>
+				<div>지도</div>
+				<div>방내놓기</div>
+			</Section>
+			<Section>
+				<div>채팅</div>
+				<div>프로필</div>
+				<div>로그아웃</div>
+			</Section>
 		</Nav>
 	);
 }
 
 const Nav = styled.nav`
 	width: 100%;
-	height: 10rem;
+	height: 5rem;
 	display: flex;
+  align-items: center;
+  justify-content: space-between;
 	border-bottom: 1px solid black;
+`;
+
+const Section = styled.section`
+	display: flex;
 `;
 
 export default Navbar;
