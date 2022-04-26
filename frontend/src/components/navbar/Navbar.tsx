a2834656d540bedbcdf4a1306b0f003770a5d4fe
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-26 15:06:37 +0900

    [S06P31D105-67] feat: 네브바 기본 구성요소 생성

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
 function Navbar() {
-  return <></>;
+	return (
+		<Nav>
+			<div>지도</div>
+			<div>방내놓기</div>
+			<div>채팅</div>
+			<div>프로필</div>
+			<div>로그아웃</div>
+		</Nav>
+	);
 }
+
+const Nav = styled.nav`
+	width: 100%;
+	height: 10rem;
+	display: flex;
+	border-bottom: 1px solid black;
+`;
 
 export default Navbar;
