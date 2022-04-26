1e7b31b68dd8d20726e654b782821082d4b2c67e
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-26 21:06:22 +0900

    [S06P31D105-67] feat: 토큰 없을 때 로그인 페이지 라우터 적용

--- frontend/src/components/navbar/rightSection/UserIcon.tsx
+++ frontend/src/components/navbar/rightSection/UserIcon.tsx
@@ -1,9 +1,11 @@
 import { faArrowRightFromBracket, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 import cookie from "react-cookies";
 import styled from "styled-components";
 
 function UserIcon() {
+	const router = useRouter();
 	const token = cookie.load("access_token");
 
 	return token ? (
@@ -12,7 +14,7 @@ function UserIcon() {
 			<FontAwesome icon={faArrowRightFromBracket} />
 		</>
 	) : (
-		<FontAwesome icon={faUser} />
+		<FontAwesome icon={faUser} onClick={() => router.push("/signin")} />
 	);
 }
 
@@ -20,6 +22,7 @@ const FontAwesome = styled(FontAwesomeIcon)`
 	width: 2rem;
 	height: 2rem;
 	margin-right: 1.5rem;
+	cursor: pointer;
 `;
 
 export default UserIcon;
