2f28e86066841f75f9b698996984d0edd1d7bcb8
우동진 <wjinh93@gmail.com>
2022-04-26 17:21:38 +0900

    [S06P31D105-31] style: padding 및 margin 변경

--- frontend/src/styles/globalstyle.ts
+++ frontend/src/styles/globalstyle.ts
@@ -2,8 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   
-  *, *::before, *::after {
+ *, *::before, *::after {
     box-sizing: border-box;
+    margin:0;
+    padding:0.2rem;
   }
   html{
     font-size: 62.5%;
