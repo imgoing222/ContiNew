e1659f157b725580a78caa3396698aae42e74445
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-26 13:39:14 +0900

    [S06P31D105-16] feat: CertifyPhoneRepository 에 deleteByExpireTimeBefore 추가

--- backend/src/main/java/com/btt/continew/member/domain/CertifyPhoneRepository.java
+++ backend/src/main/java/com/btt/continew/member/domain/CertifyPhoneRepository.java
@@ -1,8 +1,11 @@
 package com.btt.continew.member.domain;
 
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertifyPhoneRepository extends JpaRepository<CertifyPhone, Long> {
 
     int countByLoginId(String LoginId);
+
+    int deleteByExpireTimeBefore(LocalDateTime time);
 }
