e60beae53f45c1619882fb24c2e604a1701b4e76
vtaco <woosj7129@naver.com>
2022-04-28 01:35:10 +0900

    [S06P31D105-50] RedisConfig RedisMessageListener 구현

--- backend/src/main/java/com/btt/continew/global/config/RedisConfig.java
+++ backend/src/main/java/com/btt/continew/global/config/RedisConfig.java
@@ -1,12 +1,15 @@
 package com.btt.continew.global.config;
 
+import com.btt.continew.chatting.redis.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @Configuration
 public class RedisConfig {
@@ -23,11 +26,23 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory){
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chatroom");
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory,
+        MessageListenerAdapter listenerAdapter,
+        ChannelTopic channelTopic) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-
+        container.addMessageListener(listenerAdapter, channelTopic);
         return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
     }
 
     @Bean
