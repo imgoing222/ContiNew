43c5831a346155bcad704db63057b1a0d17c3954
vtaco <woosj7129@naver.com>
2022-04-28 01:36:11 +0900

    [S06P31D105-50] Chat domain 수정

--- backend/src/main/java/com/btt/continew/chatting/domain/ChatRoomRepository.java
+++ backend/src/main/java/com/btt/continew/chatting/domain/ChatRoomRepository.java
@@ -1,68 +1,37 @@
 package com.btt.continew.chatting.domain;
 
-import com.btt.continew.chatting.redis.RedisSubscriber;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 @RequiredArgsConstructor
-@Repository
+@Service
 public class ChatRoomRepository {
-
-    private final RedisMessageListenerContainer redisMessageListener;
-    private final RedisSubscriber redisSubscriber;
-
+    // Redis
     private static final String CHAT_ROOMS = "CHAT_ROOM";
-    private final RedisTemplate<String,Object> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
     private HashOperations<String, String, ChatRoom> opsHashChatRoom;
 
-    private Map<String, ChannelTopic> topics;
-
     @PostConstruct
-    private void init(){
+    private void init() {
         opsHashChatRoom = redisTemplate.opsForHash();
-        topics = new HashMap<>();
-
     }
 
-    public List<ChatRoom> findAllRoom(){
+    public List<ChatRoom> findAllRoom() {
         return opsHashChatRoom.values(CHAT_ROOMS);
     }
 
-    public ChatRoom findRoomById(String id){
-
+    public ChatRoom findRoomById(String id) {
         return opsHashChatRoom.get(CHAT_ROOMS, id);
     }
 
-    public ChatRoom createChatRoom(String name){
+    public ChatRoom createChatRoom(String name) {
         ChatRoom chatRoom = ChatRoom.create(name);
         opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
-
         return chatRoom;
-    }
-
-    public void enterChatRoom(String roomId){
-        ChannelTopic topic = topics.get(roomId);
-
-        if (topic == null){
-            topic = new ChannelTopic(roomId);
-            redisMessageListener.addMessageListener(redisSubscriber, topic);
-
-            topics.put(roomId, topic);
-        }
-    }
-
-    public ChannelTopic getTopic(String roomId){
-        return topics.get(roomId);
     }
 }
