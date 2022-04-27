51c0772f7523e05abbc54d3f09ee738ab578061a
9603100 <imgoing222@gmail.com>
2022-04-27 09:46:11 +0900

    [S06P31D105-53] feat: useForm 수정

--- frontend/src/hooks/useForm.tsx
+++ frontend/src/hooks/useForm.tsx
@@ -1,4 +1,5 @@
 import { formValidator } from "@utils/index";
+import { debounce } from "lodash";
 import { useState } from "react";
 
 interface Props {
@@ -16,15 +17,17 @@ interface Values {
 const useForm = ({ initialValues, onSubmit }: Props) => {
 	const [values, setValues] = useState(initialValues);
 	const [errors, setErrors] = useState<{ [key: string]: string }>({});
-	const [buttonDisabled, setButtonDisabled] = useState(true);
+	const [disabled, setDisabled] = useState(true);
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		setValues({ ...values, [name]: value.trim() });
-		console.log(Object(validate(values)));
-		if (Object.keys(validate(values)).length === 0) {
-			setButtonDisabled(false);
-		}
+	const handleInputChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		const { name, value } = e.target as HTMLInputElement;
+		const tempValues = { ...values, [name]: value.trim() };
+		setValues(tempValues);
+		// 에러 세팅 함수화
+		// debounce 적용 (유효성 검사 및 에러 세팅)
+		const tempErrors = validate(tempValues);
+		setErrors(tempErrors);
+		Object.keys(tempErrors).length > 0 ? setDisabled(true) : setDisabled(false);
 	};
 
 	const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -32,20 +35,20 @@ const useForm = ({ initialValues, onSubmit }: Props) => {
 		if (Object.keys(validate(values)).length === 0) {
 			return await onSubmit(values);
 		}
-		setErrors(validate(values));
 	};
 
 	const validate = ({ login_id, username, password }: Values) => {
 		const errors: { [key: string]: string } = {};
-		if (!formValidator.validateId(login_id)) errors.id = "4~20자 사이의 아이디를 입력해주세요.";
+		if (login_id && !formValidator.validateId(login_id))
+			errors.id = "4자 이상 20자 이내의 아이디를 입력해주세요.";
 		if (username && !formValidator.validateUsername(username))
-			errors.username = "2자 이상의 유저네임을 입력해주세요.";
-		if (!formValidator.validatePassword(password))
-			errors.password = "특수문자를 1자 이상 포함해주세요.";
+			errors.username = "2자 이상의 닉네임을 입력해주세요.";
+		if (password && !formValidator.validatePassword(password))
+			errors.password = "영문자, 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해주세요.";
 		return errors;
 	};
 
-	return { errors, handleFormSubmit, handleInputChange, buttonDisabled };
+	return { errors, handleFormSubmit, handleInputChange, disabled };
 };
 
 export default useForm;
