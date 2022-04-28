1e359d7426dcc7b06bf46c727e37154e1ec85991
vtaco <woosj7129@naver.com>
2022-04-29 01:20:41 +0900

    [S06P31D105-51] feat: ChatRoomRepository findAllBySellerOrBuyer 구현

--- backend/src/main/java/com/btt/continew/chatting/domain/ChatRoomRepository.java
+++ backend/src/main/java/com/btt/continew/chatting/domain/ChatRoomRepository.java
@@ -1,37 +1,42 @@
 package com.btt.continew.chatting.domain;
 
+import com.btt.continew.member.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
-@RequiredArgsConstructor
-@Service
-public class ChatRoomRepository {
+//@RequiredArgsConstructor
+public interface ChatRoomRepository extends CrudRepository<ChatRoom, String> {
+
+    Page<ChatRoom> findAllBySellerOrBuyer(Member seller, Member buyer, Pageable pageable);
     // Redis
-    private static final String CHAT_ROOMS = "CHAT_ROOM";
-    private final RedisTemplate<String, Object> redisTemplate;
-    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+//    private static final String CHAT_ROOMS = "CHAT_ROOM";
+//    private final RedisTemplate<String, Object> redisTemplate;
+//    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+//
+//    @PostConstruct
+//    private void init() {
+//        opsHashChatRoom = redisTemplate.opsForHash();
+//    }
 
-    @PostConstruct
-    private void init() {
-        opsHashChatRoom = redisTemplate.opsForHash();
-    }
+//    public List<ChatRoom> findAllRoom() {
+//        return opsHashChatRoom.values(CHAT_ROOMS);
+//    }
+//
+//    public ChatRoom findRoomById(String id) {
+//        return opsHashChatRoom.get(CHAT_ROOMS, id);
+//    }
 
-    public List<ChatRoom> findAllRoom() {
-        return opsHashChatRoom.values(CHAT_ROOMS);
-    }
-
-    public ChatRoom findRoomById(String id) {
-        return opsHashChatRoom.get(CHAT_ROOMS, id);
-    }
-
-    public ChatRoom createChatRoom(String name) {
-        ChatRoom chatRoom = ChatRoom.create(name);
-        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
-        return chatRoom;
-    }
+//    public ChatRoom createChatRoom(String name) {
+//        ChatRoom chatRoom = ChatRoom.create(name);
+//        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+//        return chatRoom;
+//    }
 }
