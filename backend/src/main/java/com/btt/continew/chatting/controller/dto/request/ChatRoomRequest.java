d25b37388d40b5012bf3574cf418bdf4eb567d66
vtaco <woosj7129@naver.com>
2022-04-29 01:14:42 +0900

    [S06P31D105-51] feat: ChatRoomRequest 생성

---
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatRoomRequest.java
@@ -0,0 +1,10 @@
+package com.btt.continew.chatting.controller.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class ChatRoomRequest {
+
+
+}
