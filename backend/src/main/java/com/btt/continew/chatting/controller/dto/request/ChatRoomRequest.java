bbd68ee7c78105405e6ef05fb9703a45d7733d1c
vtaco <woosj7129@naver.com>
2022-04-29 01:17:44 +0900

    [S06P31D105-51] feat: ChatRoomRequest 구현

--- backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatRoomRequest.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatRoomRequest.java
@@ -6,5 +6,23 @@ import lombok.Getter;
 @Getter
 public class ChatRoomRequest {
 
+    @JsonProperty("seller")
+    private String seller;
+
+    @JsonProperty("buyer")
+    private String buyer;
+
+    @JsonProperty("sale")
+    private Long sale;
+
+    public ChatRoomRequest(){
+
+    }
+
+    public ChatRoomRequest(String seller, String buyer, Long sale){
+        this.seller = seller;
+        this.buyer = buyer;
+        this.sale = sale;
+    }
 
 }
