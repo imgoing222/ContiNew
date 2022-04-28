2b3621a353256978dafebd8347e032f7f7fa15ac
우동진 <wjinh93@gmail.com>
2022-04-28 15:23:46 +0900

    [S06P31D105-92] feat: types 생성

--- frontend/src/pages/createSale/index.tsx
+++ frontend/src/pages/createSale/index.tsx
@@ -1,27 +1,8 @@
 import { Container } from "@components/account/Container";
 import SaleInfo from "@components/createSale/SaleInfo";
 import React, { useState } from "react";
+import { HouseInfo } from "src/types/houseInfo";
 import styled from "styled-components";
-
-interface HouseInfo {
-	sidoName: string;
-	gunguName: string;
-	dongName: string;
-	jibunAddress: string;
-	addressDetail: string;
-	latitude: number;
-	longitude: number;
-	floor: number;
-	saleType: string;
-	houseType: string;
-	deposit: number;
-	monthlyRent: number;
-	maintenanceFee: number;
-	maintenanceDetail: string;
-	period: number;
-	description: string;
-	option: number[];
-}
 
 export interface EventProps {
 	changeEvent: (event: React.ChangeEvent<HTMLInputElement>) => void;
---
+++ frontend/src/types/houseInfo.ts
@@ -0,0 +1,19 @@
+export interface HouseInfo {
+	sidoName: string;
+	gunguName: string;
+	dongName: string;
+	jibunAddress: string;
+	addressDetail: string;
+	latitude: number;
+	longitude: number;
+	floor: number;
+	saleType: string;
+	houseType: string;
+	deposit: number;
+	monthlyRent: number;
+	maintenanceFee: number;
+	maintenanceDetail: string;
+	period: number;
+	description: string;
+	option: number[];
+}
