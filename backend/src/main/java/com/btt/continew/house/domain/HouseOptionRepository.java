c9cf41bd26078b61d5b833f95b41b076f382c3c0
yeonju501 <loling970124@gmail.com>
2022-04-28 14:46:02 +0900

    [S06P31D105-70] feat: HouseOptionRepository deleteHouseOptionsByHouse 추가

--- backend/src/main/java/com/btt/continew/house/domain/HouseOptionRepository.java
+++ backend/src/main/java/com/btt/continew/house/domain/HouseOptionRepository.java
@@ -2,8 +2,15 @@ package com.btt.continew.house.domain;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HouseOptionRepository extends JpaRepository<HouseOption, Long> {
 
     List<HouseOption> findAllByHouse(House house);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "update houseoption ho set ho.deleted_at = current_timestamp where ho.house_id = (:id) and ho.deleted_at is null")
+    int deleteHouseOptionsByHouse(@Param("id") Long id);
 }
