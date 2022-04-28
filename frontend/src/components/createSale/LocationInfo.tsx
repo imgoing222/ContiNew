cf09b43f53bbbd2437abd0277b85305297e36360
우동진 <wjinh93@gmail.com>
2022-04-28 21:45:55 +0900

    [S06P31D105-94] feat: 다음 주소 src 추가

--- frontend/src/components/createSale/LocationInfo.tsx
+++ frontend/src/components/createSale/LocationInfo.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useRef, useState } from "react";
 import { EventProps } from "src/pages/createSale";
 import { Button, DivBox, InputText, Layout, Title } from "./index";
 import { Table, Td, Th, Tr, Ul } from "./Table";
 
 function LocationInfo({ houseInfo, changeEvent }: EventProps) {
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+		document.body.append(script);
+	}, []);
+
+	const loadLayout = () => {
+		window.daum.postcode.load(() => {
+			const postcode = new window.daum.Postcode({
+				oncomplete: function (data) {
+					console.log(data);
+				},
+			});
+			postcode.open();
+		});
+	};
+
 	return (
 		<Layout>
 			<Title>위치 정보</Title>
@@ -15,7 +33,7 @@ function LocationInfo({ houseInfo, changeEvent }: EventProps) {
 								<li>
 									<DivBox>
 										<InputText width={40} />
-										<Button>주소 검색</Button>
+										<Button onClick={loadLayout}>주소 검색</Button>
 									</DivBox>
 								</li>
 							</Ul>
