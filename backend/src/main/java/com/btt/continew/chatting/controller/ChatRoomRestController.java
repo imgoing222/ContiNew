faca077543dc21c4ab3c5d02435452f0369b05bd
vtaco <woosj7129@naver.com>
2022-04-29 01:21:27 +0900

    [S06P31D105-51] feat: ChatRoomRestController getRoom 구현 - 채팅방 목록 요청

--- backend/src/main/java/com/btt/continew/chatting/controller/ChatRoomRestController.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/ChatRoomRestController.java
@@ -26,6 +26,15 @@ public class ChatRoomRestController {
 
     private final ChatService chatService;
 
+    @GetMapping("/room")
+    @ResponseBody
+    public ResponseEntity<ChatRoomsResponse> getRoom(
+        @PageableDefault(sort = "last_message_time", direction = Direction.DESC)Pageable pageable,
+        @ApiParam(hidden = true) @AuthenticationPrincipal String loginId
+    ) {
+        System.out.println("겟룸");
+        return ResponseEntity.ok().body(chatService.showChatRoom(pageable,loginId));
+    }
 
     @PostMapping("/room")
     @ResponseBody
@@ -33,7 +42,5 @@ public class ChatRoomRestController {
         System.out.println("포스트 룸");
         return chatService.createChatRoom(request);
     }
-
-
 
 }
