3504835d46fc23199208658bb6a3b42823fe4e7c
우동진 <wjinh93@gmail.com>
2022-04-28 21:35:29 +0900

    [S06P31D105-94] feat: 주소 input 및버튼 추가

--- frontend/src/components/createSale/LocationInfo.tsx
+++ frontend/src/components/createSale/LocationInfo.tsx
@@ -1,5 +1,30 @@
-function LocationInfo() {
-	return <div>LocationInfo</div>;
+import { EventProps } from "src/pages/createSale";
+import { Button, DivBox, InputText, Layout, Title } from "./index";
+import { Table, Td, Th, Tr, Ul } from "./Table";
+
+function LocationInfo({ houseInfo, changeEvent }: EventProps) {
+	return (
+		<Layout>
+			<Title>위치 정보</Title>
+			<Table>
+				<tbody>
+					<Tr>
+						<Th>주소</Th>
+						<Td>
+							<Ul>
+								<li>
+									<DivBox>
+										<InputText width={40} />
+										<Button>주소 검색</Button>
+									</DivBox>
+								</li>
+							</Ul>
+						</Td>
+					</Tr>
+				</tbody>
+			</Table>
+		</Layout>
+	);
 }
 
 export default LocationInfo;
