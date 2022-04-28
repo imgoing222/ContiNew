3360fd1d8b305e5ceb7f63d11e836362f72e7322
우동진 <wjinh93@gmail.com>
2022-04-28 23:09:03 +0900

    [S06P31D105-94] feat: 주소 검색후 좌표 얻는 함수 추가

--- frontend/src/components/createSale/LocationInfo.tsx
+++ frontend/src/components/createSale/LocationInfo.tsx
@@ -1,20 +1,35 @@
 import { useEffect, useRef, useState } from "react";
 import { EventProps } from "src/pages/createSale";
 import { Button, DivBox, InputText, Layout, Title } from "./index";
-import { Table, Td, Th, Tr, Ul } from "./Table";
-
+import { Table, Td, Text, Th, Tr, Ul } from "./Table";
+import axios from "axios";
 function LocationInfo({ houseInfo, changeEvent }: EventProps) {
+	const [address, setAddress] = useState("");
 	useEffect(() => {
 		const script = document.createElement("script");
 		script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
 		document.body.append(script);
 	}, []);
 
+	const searchCoordinate = async () => {
+		const result = await axios.get(
+			"https://dapi.kakao.com/v2/local/search/address.json?query=산격1동",
+			{
+				headers: {
+					Authorization: `KakaoAK cac93a0239e06151f08da334787b2819`,
+				},
+			},
+		);
+		console.log(result.data.documents[0].x);
+		console.log(result.data.documents[0].y);
+		return result;
+	};
 	const loadLayout = () => {
 		window.daum.postcode.load(() => {
 			const postcode = new window.daum.Postcode({
 				oncomplete: function (data) {
-					console.log(data);
+					searchCoordinate();
+					setAddress(data.jibunAddress);
 				},
 			});
 			postcode.open();
@@ -32,8 +47,45 @@ function LocationInfo({ houseInfo, changeEvent }: EventProps) {
 							<Ul>
 								<li>
 									<DivBox>
-										<InputText width={40} />
+										<InputText
+											width={40}
+											value={address}
+											name="jibunAddress"
+											placeholder="예) 서울 동대문구 이문동 294-295"
+											onChange={changeEvent}
+											readOnly
+										/>
 										<Button onClick={loadLayout}>주소 검색</Button>
+									</DivBox>
+								</li>
+							</Ul>
+						</Td>
+						<Td>
+							<Ul>
+								<li>
+									<DivBox>
+										<InputText
+											value={houseInfo.addressDetail}
+											name="addressDetail"
+											placeholder="상세 주소"
+											onChange={changeEvent}
+										/>
+									</DivBox>
+								</li>
+							</Ul>
+						</Td>
+						<Td>
+							<Ul>
+								<li>
+									<DivBox>
+										<InputText
+											width={10}
+											value={houseInfo.floor}
+											name="floor"
+											placeholder="층수"
+											onChange={changeEvent}
+										/>
+										<Text>층</Text>
 									</DivBox>
 								</li>
 							</Ul>
