df66af5c8007d9105f0ec6827c206796fbcd8a13
우동진 <wjinh93@gmail.com>
2022-04-28 20:47:36 +0900

    [S06P31D105-93] feat: InputCheck 구현

--- frontend/src/components/createSale/OptionInfo.tsx
+++ frontend/src/components/createSale/OptionInfo.tsx
@@ -1,8 +1,11 @@
 import Layout from "./Layout";
+import Title from "./Title";
 
 function OptionInfo() {
-	return <Layout>
-    
-  </Layout>
-
+	return (
+		<Layout>
+			<Title>옵션 정보</Title>
+		</Layout>
+	);
+}
 export default OptionInfo;
