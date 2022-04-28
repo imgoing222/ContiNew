248d2e29aa6478412d16aef772ed8fe83e094166
vtaco <woosj7129@naver.com>
2022-04-29 01:15:26 +0900

    [S06P31D105-51] feat: ChatRoomsResponse 생성

---
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/response/ChatRoomsResponse.java
@@ -0,0 +1,13 @@
+package com.btt.continew.chatting.controller.dto.response;
+
+import com.btt.continew.chatting.domain.ChatRoom;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+@Getter
+public class ChatRoomsResponse {
+
+}
