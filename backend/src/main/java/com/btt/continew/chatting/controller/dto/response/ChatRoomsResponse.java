1a141340948dd379fa77a5bc8670a8a5fd272499
vtaco <woosj7129@naver.com>
2022-04-29 01:19:53 +0900

    [S06P31D105-51] feat: ChatRoomsResponse 구현

--- backend/src/main/java/com/btt/continew/chatting/controller/dto/response/ChatRoomsResponse.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/response/ChatRoomsResponse.java
@@ -10,4 +10,32 @@ import org.springframework.data.domain.Page;
 @Getter
 public class ChatRoomsResponse {
 
+    @JsonProperty("chat_rooms")
+    private List<ChatRoomResponse> chatRooms;
+
+    @JsonProperty("total_page_count")
+    private int totalPageCount;
+
+    @JsonProperty("current_page_count")
+    private int currentPageCount;
+
+    public ChatRoomsResponse(){
+
+    }
+
+    public ChatRoomsResponse(List<ChatRoomResponse> chatRooms, int totalPageCount, int currentPageCount){
+        this.chatRooms = chatRooms;
+        this.totalPageCount = totalPageCount;
+        this.currentPageCount = currentPageCount;
+    }
+
+    public static ChatRoomsResponse from(Page<ChatRoom> chatRooms){
+        return new ChatRoomsResponse(
+            chatRooms.stream()
+                .map(ChatRoomResponse::from)
+                .collect(Collectors.toList()),
+            chatRooms.getTotalPages(),
+            chatRooms.getNumber()
+        );
+    }
 }
