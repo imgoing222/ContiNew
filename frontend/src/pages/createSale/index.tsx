b231f034e81f7665fb76a4983a56d9fa778ba744
우동진 <wjinh93@gmail.com>
2022-04-28 12:50:03 +0900

    [S06P31D105-91] feat: container 추가

--- frontend/src/pages/createSale/index.tsx
+++ frontend/src/pages/createSale/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Container } from "@components/account/Container";
+import SaleInfo from "@components/createSale/SaleInfo";
+import styled from "styled-components";
 
 function index() {
-	return <div>index</div>;
+	return (
+		<Container>
+			<SaleInfo />
+		</Container>
+	);
 }
 
 export default index;
+
+const Conatiner = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	min-width: 120rem;
+`;
