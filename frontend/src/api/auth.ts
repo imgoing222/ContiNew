1c81a82a1a0683549d891a11a94cf67ac425bd12
9603100 <imgoing222@gmail.com>
2022-04-29 11:42:48 +0900

    [S06P31D105-98] feat: 휴대폰 인증 번호 확인 함수 구현

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from "axios";
+import { Axios, AxiosResponse } from "axios";
 import { request } from "./request";
 
 interface AuthApiType {
@@ -9,12 +9,14 @@ interface AuthApiType {
 	}) => Promise<AxiosResponse>;
 	signin: (userInfo: { login_id: string; password: string }) => Promise<AxiosResponse>;
 	sendCode: (phoneNumber: { phone_number: string }) => Promise<AxiosResponse>;
+	confirmCode: (code: { code: string }) => Promise<AxiosResponse>;
 }
 
 const authApi: AuthApiType = {
 	signup: (userInfo) => request.post("members", userInfo),
 	signin: (userInfo) => request.post("members/login", userInfo),
 	sendCode: (phoneNumber) => request.post("auth/members/phone-send", phoneNumber),
+	confirmCode: (code) => request.post("auth/members/phone-check", code),
 };
 
 export default authApi;
--- frontend/src/pages/smsVerification.tsx
+++ frontend/src/pages/smsVerification.tsx
@@ -9,14 +9,29 @@ import authApi from "src/api/auth";
 
 function smsVerification() {
 	const [phoneNumber, setPhoneNumber] = useState({ phone_number: "" });
+	const [code, setCode] = useState({ code: "" });
+	const [disabled, setDisabled] = useState(true);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setPhoneNumber({ phone_number: e.target.value });
+		if (e.target.name === "phoneNumber") setPhoneNumber({ phone_number: e.target.value });
+		else if (e.target.name === "code") setCode({ code: e.target.value });
+		console.log(phoneNumber, code);
 	};
 
 	const handleSendClick = async () => {
 		try {
 			const res = await authApi.sendCode(phoneNumber);
+			console.log(res);
+			setDisabled(false);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const handleConfirmClick = async () => {
+		try {
+			const res = await authApi.confirmCode(code);
+			console.log(code);
 			console.log(res);
 		} catch (err) {
 			console.log(err);
@@ -28,13 +43,15 @@ function smsVerification() {
 			<Header>휴대폰 인증</Header>
 			<Label>휴대폰 번호</Label>
 			<div>
-				<Input placeholder="-없이 숫자만 입력" onChange={handleInputChange} />
+				<Input placeholder="-없이 숫자만 입력" name="phoneNumber" onChange={handleInputChange} />
 				<Button onClick={handleSendClick}>인증번호 전송</Button>
 			</div>
 			<Label>인증 번호</Label>
 			<div>
-				<Input placeholder="인증 번호 입력" />
-				<Button>인증번호 확인</Button>
+				<Input placeholder="인증 번호 입력" name="code" onChange={handleInputChange} />
+				<Button disabled={disabled} onClick={handleConfirmClick}>
+					인증번호 확인
+				</Button>
 			</div>
 			<LinkButton href="/">본인 인증 다음에 하기</LinkButton>
 		</Container>
