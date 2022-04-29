9266dbad7a82885f822be97f9ed5ecfda8699e2a
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-29 17:29:19 +0900

    [S06P31D105-43] feat: createChat Api 추가

--- frontend/src/api/chat.ts
+++ frontend/src/api/chat.ts
@@ -1,6 +1,18 @@
 import { AxiosResponse } from "axios";
 import { request } from "./request";
 
-const chatApi = {};
+interface ChatData {
+  buyer: string;
+  seller: string;
+  sale: number;
+}
+
+interface ChatApiType {
+  createChat: (chatData: ChatData) => Promise<AxiosResponse>;
+}
+
+const chatApi: ChatApiType = {
+  createChat: (chatData) => request.post("chat/room", chatData),
+};
 
 export default chatApi;
