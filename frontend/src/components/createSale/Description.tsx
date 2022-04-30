9dfbe4c44e0cb0a1c9dabd44d3eea473c7758f26
우동진 <wjinh93@gmail.com>
2022-04-30 22:26:31 +0900

    [S06P31D105-95] feat: description 컴포넌트 생성

---
+++ frontend/src/components/createSale/Description.tsx
@@ -0,0 +1,13 @@
+import Layout from "./Layout";
+import Title from "./Title";
+
+function Description() {
+	return (
+		<Layout>
+			<Title>상세 설명</Title>
+			<textarea name="description" id="" cols="30" rows="10"></textarea>
+		</Layout>
+	);
+}
+
+export default Description;
