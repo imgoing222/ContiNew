af7f21037d84dff5bf941af8ad6b3dca10475329
우동진 <wjinh93@gmail.com>
2022-04-30 22:22:26 +0900

    [S06P31D105-94] feat: 주소 검색후 useState에 저장

--- frontend/src/components/createSale/LocationInfo.tsx
+++ frontend/src/components/createSale/LocationInfo.tsx
@@ -3,31 +3,33 @@ import { EventProps } from "src/pages/createSale";
 import { Button, DivBox, InputText, Layout, Title } from "./index";
 import { Table, Td, Text, Th, Tr, Ul } from "./Table";
 import axios from "axios";
-function LocationInfo({ houseInfo, changeEvent }: EventProps) {
+function LocationInfo({ houseInfo, changeEvent, setHouseInfo }: EventProps) {
 	useEffect(() => {
 		const script = document.createElement("script");
 		script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
 		document.body.append(script);
 	}, []);
 
-	const searchCoordinate = async () => {
+	const searchCoordinate = async (data: DaumPostcodeData) => {
 		const result = await axios.get(
-			"https://dapi.kakao.com/v2/local/search/address.json?query=산격1동",
+			`https://dapi.kakao.com/v2/local/search/address.json?query=${data.address}`,
 			{
 				headers: {
 					Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API}`,
 				},
 			},
 		);
-		const longitude = result.data.documents[0].x;
-		const latitude = result.data.documents[0].y;
+		const { jibunAddress, sido, sigungu, bname } = data;
+		const longitude = +result.data.documents[0].x;
+		const latitude = +result.data.documents[0].y;
+		if (setHouseInfo)
+			setHouseInfo({ ...houseInfo, longitude, latitude, jibunAddress, sido, sigungu, bname });
 	};
 	const loadLayout = () => {
 		window.daum.postcode.load(() => {
 			const postcode = new window.daum.Postcode({
 				oncomplete: function (data) {
-					// searchCoordinate();
-					console.log(data.jibunAddress);
+					searchCoordinate(data);
 				},
 			});
 			postcode.open();
