210bfb3ba6ddc5eaaed4e9688694c2706aea8b41
우동진 <wjinh93@gmail.com>
2022-04-30 22:31:48 +0900

    [S06P31D105-95] feat: textarea 추가

--- frontend/src/components/createSale/Description.tsx
+++ frontend/src/components/createSale/Description.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Layout from "./Layout";
 import Title from "./Title";
 
@@ -5,9 +6,20 @@ function Description() {
 	return (
 		<Layout>
 			<Title>상세 설명</Title>
-			<textarea name="description" id="" cols="30" rows="10"></textarea>
+			<Textarea name="description" />
 		</Layout>
 	);
 }
 
 export default Description;
+
+const Textarea = styled.textarea`
+	width: 100%;
+	height: 20rem;
+	font-size: 1.3rem;
+	padding: 1rem;
+	outline: none;
+	border: none;
+	border-top: 1px solid ${(props) => props.theme.borderColor};
+	resize: none;
+`;
