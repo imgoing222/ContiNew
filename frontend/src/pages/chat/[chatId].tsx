35ad346b5d10165fbdc7931e949cfdaddd6ffd4b
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-01 14:57:36 +0900

    [S06P31D105-102] refactor: 필요없는 코드 삭제

--- frontend/src/pages/chat/[chatId].tsx
+++ frontend/src/pages/chat/[chatId].tsx
@@ -24,30 +24,6 @@ function ChatDetail() {
 		});
 	});
 
-	// const webSocketUrl = "ws://localhost:8080/ws/chat";
-	// let ws = useRef<WebSocket | null>(null);
-
-	// useEffect(() => {
-	// 	if (!ws.current) {
-	// 		ws.current = new WebSocket(webSocketUrl);
-	// 		ws.current.onopen = () => {
-	// 			console.log("connected to " + webSocketUrl);
-	// 			setSocketConnected(true);
-	// 		};
-	// 		ws.current.onclose = () => {
-	// 			console.log("disconnect from " + webSocketUrl);
-	// 		};
-	// 		ws.current.onerror = (error) => {
-	// 			console.log("connenction error " + webSocketUrl);
-	// 			console.log(error);
-	// 		};
-	// 		ws.current.onmessage = (evnet) => {
-	// 			const data = JSON.parse(evnet.data);
-	// 			console.log(data);
-	// 		};
-	// 	}
-	// });
-
 	return (
 		<Container>
 			<ChatList />
