37dcddd3e2399ed40ede1b75bcfcd1b21e73a18c
9603100 <imgoing222@gmail.com>
2022-05-02 14:08:53 +0900

    [S06P31D105-100] feat: 타이머 컴포넌트 구현

--- frontend/src/components/account/Timer.tsx
+++ frontend/src/components/account/Timer.tsx
@@ -1,6 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+
 function Timer() {
-	const startTimer = setInterval(() => {}, 1000);
-	return;
+	const [min, setMin] = useState(3);
+	const [sec, setSec] = useState(0);
+	const time = useRef(180);
+	const timerId = useRef<any>(null);
+
+	useEffect(() => {
+		timerId.current = setInterval(() => {
+			setMin(time.current / 60);
+			setSec(time.current % 60);
+			time.current -= 1;
+		}, 1000);
+
+		return () => clearInterval(timerId.current);
+	}, []);
+
+	useEffect(() => {
+		if (time.current <= 0) {
+			alert("타임 아웃");
+			clearInterval(timerId.current);
+		}
+	}, [sec]);
+
+	return (
+		<div>
+			{min} 분 {sec} 초
+		</div>
+	);
 }
 
 export default Timer;
