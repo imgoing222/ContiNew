b0439a0d4e6ca8e637e3c240b2495c4e9454c23c
우동진 <wjinh93@gmail.com>
2022-05-02 14:11:26 +0900

    [S06P31D105-96] feat: 설명란 구현

--- frontend/src/components/createSale/Photos.tsx
+++ frontend/src/components/createSale/Photos.tsx
@@ -1,12 +1,36 @@
+import { EventProps } from "src/pages/createSale";
+import styled from "styled-components";
 import Layout from "./Layout";
 import Title from "./Title";
 
-function Photos({ houseInfo, changeEvent }) {
+interface ContainerProps {
+	height?: number;
+}
+
+function Photos({ houseInfo, changeEvent }: EventProps) {
 	return (
 		<Layout>
 			<Title>사진 등록</Title>
+			<Container>
+				<Text>- 사진은 최소 3장 최대 10장 까지 업로드가 가능합니다.</Text>
+				<Text>- 사진 용량은 최대 100MB까지 가능합니다.</Text>
+				<Text>- 사진은 가로로 찍은 사진을 권장합니다.</Text>
+			</Container>
+			<Container></Container>
 		</Layout>
 	);
 }
 
 export default Photos;
+
+const Container = styled.div<ContainerProps>`
+	width: calc(100% - 2rem);
+	height: ${({ height }) => (height ? `${height}rem` : "auto")};
+	margin: 1rem;
+	padding: 2rem;
+	border: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const Text = styled.p`
+	font-size: 1.3rem;
+`;
