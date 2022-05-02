83183d408e8d311362de80bc626731ce06972e3d
9603100 <imgoing222@gmail.com>
2022-05-02 14:31:45 +0900

    [S06P31D105-100] feat: 타이머 컴포넌트 인증 페이지에 추가 및 로직 수정

--- frontend/src/components/account/Timer.tsx
+++ frontend/src/components/account/Timer.tsx
@@ -8,7 +8,7 @@ function Timer() {
 
 	useEffect(() => {
 		timerId.current = setInterval(() => {
-			setMin(time.current / 60);
+			setMin(Math.floor(time.current / 60));
 			setSec(time.current % 60);
 			time.current -= 1;
 		}, 1000);
@@ -17,15 +17,16 @@ function Timer() {
 	}, []);
 
 	useEffect(() => {
-		if (time.current <= 0) {
-			alert("타임 아웃");
+		if (time.current < 0) {
+			alert("인증시간이 경과되었습니다");
 			clearInterval(timerId.current);
+			window.location.reload();
 		}
 	}, [sec]);
 
 	return (
 		<div>
-			{min} 분 {sec} 초
+			{min > 0 && `${min}분`} {sec}초
 		</div>
 	);
 }
