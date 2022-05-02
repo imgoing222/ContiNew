494f3802f23961d4d0a253f8a2765cb463d4a3d3
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-02 10:18:05 +0900

    [S06P31D105-106] refactor: 더미데이터 삭제

--- frontend/src/pages/chat/index.tsx
+++ frontend/src/pages/chat/index.tsx
@@ -4,30 +4,6 @@ import styled from "styled-components";
 import { chatApi } from "src/api";
 import { Chat, ChatList, ItemDetail } from "@components/chat";
 
-const DUMMY_DATA = [
-	{
-		chatId: 1,
-		user: "김싸피",
-		person: "이싸피",
-		lastContent: "화장실 사진 좀 찍어주세요.",
-		updatedAt: "2022-04-20 14:22:04",
-	},
-	{
-		chatId: 2,
-		user: "박싸피",
-		person: "유싸피",
-		lastContent: "벌레는 안나오나요 ?",
-		updatedAt: "2022-04-19 18:58:22",
-	},
-	{
-		chatId: 3,
-		user: "조싸피",
-		person: "이싸피",
-		lastContent: "깔끔하고 좋아요 !!",
-		updatedAt: "2022-04-19 08:02:39",
-	},
-];
-
 function ChatPage() {
 	const [chatListData, setChatListData] = useState({
 		chat_rooms: [
