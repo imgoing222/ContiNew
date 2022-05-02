c2d0c66194834ea3f383d1951c79dea8d27100b6
vtaco <woosj7129@naver.com>
2022-05-02 11:05:39 +0900

    [S06P31D105-51] feat: ChatRoomResponse Swagger

--- backend/src/main/java/com/btt/continew/chatting/controller/dto/response/ChatRoomsResponse.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/response/ChatRoomsResponse.java
@@ -2,6 +2,7 @@ package com.btt.continew.chatting.controller.dto.response;
 
 import com.btt.continew.chatting.domain.ChatRoom;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.Getter;
@@ -11,12 +12,15 @@ import org.springframework.data.domain.Page;
 public class ChatRoomsResponse {
 
     @JsonProperty("chat_rooms")
+    @ApiModelProperty(position = 0, notes = "채팅 방 목록 목록")
     private List<ChatRoomResponse> chatRooms;
 
     @JsonProperty("total_page_count")
+    @ApiModelProperty(position = 1, notes = "채팅방 총 페이지 수", example = "2")
     private int totalPageCount;
 
     @JsonProperty("current_page_count")
+    @ApiModelProperty(position = 2, notes = "채팅방 현재 페이지", example = "1")
     private int currentPageCount;
 
     public ChatRoomsResponse(){
