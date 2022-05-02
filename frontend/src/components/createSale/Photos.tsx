72f13d0aab2047279f97f22122abbd859fe357f0
우동진 <wjinh93@gmail.com>
2022-05-02 14:31:32 +0900

    [S06P31D105-96] feat: 파일 input 구현

--- frontend/src/components/createSale/Photos.tsx
+++ frontend/src/components/createSale/Photos.tsx
@@ -5,6 +5,8 @@ import Title from "./Title";
 
 interface ContainerProps {
 	height?: number;
+	background?: boolean;
+	border?: boolean;
 }
 
 function Photos({ houseInfo, changeEvent }: EventProps) {
@@ -16,7 +18,10 @@ function Photos({ houseInfo, changeEvent }: EventProps) {
 				<Text>- 사진 용량은 최대 100MB까지 가능합니다.</Text>
 				<Text>- 사진은 가로로 찍은 사진을 권장합니다.</Text>
 			</Container>
-			<Container></Container>
+			<Container height={32} background={true}>
+				<Button htmlFor="images">사진 업로드하기</Button>
+				<Input type="file" id="images" />
+			</Container>
 		</Layout>
 	);
 }
@@ -28,9 +33,34 @@ const Container = styled.div<ContainerProps>`
 	height: ${({ height }) => (height ? `${height}rem` : "auto")};
 	margin: 1rem;
 	padding: 2rem;
-	border: 1px solid ${(props) => props.theme.borderColor};
+	border: ${(props) => (props.background ? "none" : `1px solid ${props.theme.borderColor}`)};
+	background-color: ${({ background }) => (background ? `rgb(244,244,244)` : "none")};
+	position: relative;
 `;
 
 const Text = styled.p`
 	font-size: 1.3rem;
+`;
+
+const Button = styled.label`
+	width: 12rem;
+	height: 4rem;
+	cursor: pointer;
+	background-color: ${(props) => props.theme.mainColor};
+	border: none;
+	border-radius: 0.5rem;
+	font-size: 1.3rem;
+	font-weight: 800;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #fff;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
+
+const Input = styled.input`
+	display: none;
 `;
