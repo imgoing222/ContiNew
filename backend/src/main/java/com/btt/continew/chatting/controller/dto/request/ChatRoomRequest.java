2fe6b9f945452a1dcf53dc50bb1e64b86cf81867
vtaco <woosj7129@naver.com>
2022-05-02 11:05:49 +0900

    [S06P31D105-51] feat: ChatRoomRequest Swagger

--- backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatRoomRequest.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatRoomRequest.java
@@ -1,18 +1,22 @@
 package com.btt.continew.chatting.controller.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class ChatRoomRequest {
 
     @JsonProperty("seller")
+    @ApiModelProperty(position = 0, notes = "판매자", example = "ti-a-co")
     private String seller;
 
     @JsonProperty("buyer")
+    @ApiModelProperty(position = 1, notes = "구매자", example = "bbang")
     private String buyer;
 
     @JsonProperty("sale")
+    @ApiModelProperty(position = 2, notes = "매물 번호", example = "1")
     private Long sale;
 
     public ChatRoomRequest(){
