8fc87b52831ac9bb207b19432120508e8c6e0a76
우동진 <wjinh93@gmail.com>
2022-05-02 13:54:21 +0900

    [S06P31D105-96] feat: props 추가

--- frontend/src/components/createSale/Photos.tsx
+++ frontend/src/components/createSale/Photos.tsx
@@ -1,5 +1,12 @@
-function Photos() {
-	return <div>Photos</div>;
+import Layout from "./Layout";
+import Title from "./Title";
+
+function Photos({ houseInfo, changeEvent }) {
+	return (
+		<Layout>
+			<Title>사진 등록</Title>
+		</Layout>
+	);
 }
 
 export default Photos;
