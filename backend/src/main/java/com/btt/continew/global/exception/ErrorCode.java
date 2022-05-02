a69276dbcaa50da546e3446da1b5556223ea6689
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-02 10:25:24 +0900

    [S06P310105-15] feat: ErrorCode 에 구글 로그인 관련 에러 추가

--- backend/src/main/java/com/btt/continew/global/exception/ErrorCode.java
+++ backend/src/main/java/com/btt/continew/global/exception/ErrorCode.java
@@ -23,6 +23,10 @@ public enum ErrorCode {
     INVALID_UNSUPPORTED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 리프레시 토큰입니다.", "J13"),
     INVALID_ILLEGAL_ARGUMENT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레쉬 토큰이 잘못되었습니다.", "J14"),
 
+    // login
+    LOGIN_GOOGLE_RESPONSE_FAIL(HttpStatus.BAD_REQUEST, "구글 요청을 얻는데 실패했습니다.", "L01"),
+    LOGIN_GOOGLE_USER_INFO_RESPONSE_FAIL(HttpStatus.BAD_REQUEST, "구글 유저 정보를 얻는데 실패했습니다.", "L02"),
+
     // member
     MEMBER_NOT_FOUND_BY_LOGIN_ID(HttpStatus.NOT_FOUND, "존재하지 않는 로그인 아이디입니다.", "M01"),
     MEMBER_LOGIN_ID_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 로그인 아이디 입니다.", "M02"),
@@ -30,6 +34,7 @@ public enum ErrorCode {
     MEMBER_LOGIN_ERROR_BY_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다", "M04"),
     MEMBER_USERNAME_DUPLICATED_BECAUSE_OF_YOU(HttpStatus.CONFLICT, "회원님이 이미 사용 중인 닉네임입니다.", "M05"),
     MEMBER_PHONE_NUMBER_DUPLICATED(HttpStatus.CONFLICT, "이미 인증된 전화번호 입니다.", "M06"),
+    MEMBER_NOT_SOCIAL_USER(HttpStatus.CONFLICT, "해당 회원은 소셜 아이디 로그인 회원이 아닙니다.", "M07"),
 
     // certify
     CERTIFY_NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 아이디로 인증 요청을 한 기록이 없습니다.", "I01"),
