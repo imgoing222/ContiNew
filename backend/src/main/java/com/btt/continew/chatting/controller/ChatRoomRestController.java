090c0f195a806a22e92535ab57698abeb63ed1fd
vtaco <woosj7129@naver.com>
2022-05-02 11:06:34 +0900

    [S06P31D105-51] feat: ChatRoomRestController swagger

--- backend/src/main/java/com/btt/continew/chatting/controller/ChatRoomRestController.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/ChatRoomRestController.java
@@ -2,9 +2,14 @@ package com.btt.continew.chatting.controller;
 
 
 import com.btt.continew.chatting.controller.dto.request.ChatRoomRequest;
+import com.btt.continew.chatting.controller.dto.response.ChatRoomResponse;
 import com.btt.continew.chatting.controller.dto.response.ChatRoomsResponse;
 import com.btt.continew.chatting.domain.ChatRoom;
 import com.btt.continew.chatting.service.ChatService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -19,12 +24,15 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
+@Api(tags = {"채팅방"})
 @RequestMapping("/api/chat")
 public class ChatRoomRestController {
 
     private final ChatService chatService;
 
     @GetMapping("/room")
+    @ApiOperation(value = "채팅방 목록 조회", notes = "<b>(로그인 필요)</b> 채팅방 목록 조회 API API")
+
     @ResponseBody
     public ResponseEntity<ChatRoomsResponse> getRoom(
         @PageableDefault(sort = "last_message_time", direction = Direction.DESC)Pageable pageable,
@@ -35,6 +43,7 @@ public class ChatRoomRestController {
     }
 
     @PostMapping("/room")
+    @ApiOperation(value = "채팅방 생성", notes = "<b>(로그인 필요)</b> 채팅방 생성 API")
     @ResponseBody
     public ChatRoom postRoom(@RequestBody ChatRoomRequest request) {
         System.out.println("포스트 룸");
@@ -42,7 +51,10 @@ public class ChatRoomRestController {
     }
 
     @GetMapping("/room/{room_id}")
-    public ChatRoom getRoomDetail(
+    @ApiOperation(value = "채팅방 조회", notes = "<b>(로그인 필요)</b> 채팅방 조회 API")
+    @ApiImplicitParam(name = "room_id", value = "방 번호",example = "e1757da8-81c5-40e4-8fe0-f22d4031f6a4",required = true,type = "REQEUST_HEADER")
+    @ResponseBody
+    public ChatRoomResponse getRoomDetail(
         @RequestParam(value = "room_id") String roomId){
 
         System.out.println("겟룸 디테일");
