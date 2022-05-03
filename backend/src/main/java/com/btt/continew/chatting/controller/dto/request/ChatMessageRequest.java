3d4277859692d2a8f572860d103a9de968d11a64
vtaco <woosj7129@naver.com>
2022-05-03 16:50:48 +0900

    [S06P31D105-51] feat: ChatMessageRequest 구현 - 채팅 메시지 요청 처리

--- backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatMessageRequest.java
+++ backend/src/main/java/com/btt/continew/chatting/controller/dto/request/ChatMessageRequest.java
@@ -1,5 +1,32 @@
 package com.btt.continew.chatting.controller.dto.request;
 
+import com.btt.continew.chatting.domain.ChatMessage.MessageType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
 public class ChatMessageRequest {
 
+    @JsonProperty("message_type")
+    MessageType type;
+
+    @JsonProperty("room_id")
+    String roomId;
+
+    @JsonProperty("sender")
+    String sender;
+
+    @JsonProperty("content")
+    String content;
+
+    public ChatMessageRequest(){
+
+    }
+
+    public ChatMessageRequest(MessageType type, String roomId, String sender, String content){
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.content = content;
+    }
 }
