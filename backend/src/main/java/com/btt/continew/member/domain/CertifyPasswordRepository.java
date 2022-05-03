1d2748ef5a9bf84565047a17ccaba7d9473ca910
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-03 11:45:23 +0900

    [S06P310105-108] feat: CertifyPasswordRepository 생성

---
+++ backend/src/main/java/com/btt/continew/member/domain/CertifyPasswordRepository.java
@@ -0,0 +1,7 @@
+package com.btt.continew.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CertifyPasswordRepository extends JpaRepository<CertifyPhone, Long> {
+
+}
