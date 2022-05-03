7b9c77797a4d1d7116776fb4d3d479023a3a29b2
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-03 12:35:16 +0900

    [S06P310105-108] feat: CertifyPassword 구조 변경

--- backend/src/main/java/com/btt/continew/member/domain/CertifyPassword.java
+++ backend/src/main/java/com/btt/continew/member/domain/CertifyPassword.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
@@ -16,7 +17,7 @@ public class CertifyPassword {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "certify_phone_id")
+    @Column(name = "certify_password_id")
     private Long id;
 
     @Column(name = "certification_code", length = 6)
@@ -24,6 +25,9 @@ public class CertifyPassword {
 
     @Column(name = "change_token", length = 12)
     private String changeToken;
+
+    @Column(name = "expired")
+    private Boolean expired;
 
     @ManyToOne(targetEntity = Member.class)
     @JoinColumn(name = "member_id", nullable = false)
@@ -34,4 +38,19 @@ public class CertifyPassword {
 
     @Column(name = "today_count")
     private Integer todayCount;
+
+    @Builder
+    public CertifyPassword(String certificationCode, String changeToken, Boolean expired,
+        Member member, LocalDateTime expireTime, Integer todayCount) {
+        this.certificationCode = certificationCode;
+        this.changeToken = changeToken;
+        this.expired = expired;
+        this.member = member;
+        this.expireTime = expireTime;
+        this.todayCount = todayCount;
+    }
+
+    public CertifyPassword() {
+
+    }
 }
