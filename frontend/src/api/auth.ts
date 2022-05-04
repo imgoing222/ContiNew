d4de756ad96f50d21b553ea1c1a5d5bf5dab8ff7
9603100 <imgoing222@gmail.com>
2022-05-04 14:26:19 +0900

    [S06P31D105-116] feat: 구글 로그인 임시 수정

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -11,8 +11,6 @@ interface AuthApiType {
 	reissue: (tokens: { access_token: string; refresh_token: string }) => Promise<AxiosResponse>;
 	sendCode: (phoneNumber: { phone_number: string }) => Promise<AxiosResponse>;
 	confirmCode: (code: { code: string }) => Promise<AxiosResponse>;
-	google: () => Promise<AxiosResponse>;
-	googleLogin: (code: string | string[] | undefined) => Promise<AxiosResponse>;
 }
 
 const authApi: AuthApiType = {
@@ -21,8 +19,6 @@ const authApi: AuthApiType = {
 	reissue: (tokens) => authRequest.post("members/reissue", tokens),
 	sendCode: (phoneNumber) => request.post("auth/members/phone-send", phoneNumber),
 	confirmCode: (code) => request.post("auth/members/phone-check", code),
-	google: () => request.get("members/login/google"),
-	googleLogin: (code) => request.get(`members/login/google/callback?code=${code}`),
 };
 
 export default authApi;
