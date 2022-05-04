71a50d45888e157d88e5b8c99e4d6ef40e43d67f
vtaco <woosj7129@naver.com>
2022-05-04 18:12:52 +0900

    [S06P31D105-51] refactor: ChatMessage* - feature trace

--- backend/src/main/java/com/btt/continew/chatting/service/ChatMessageService.java
+++ backend/src/main/java/com/btt/continew/chatting/service/ChatMessageService.java
@@ -27,34 +27,39 @@ public class ChatMessageService {
     @PostConstruct
     private void init() { opsHashChatMessage = redisTemplate.opsForHash(); }
 
+
     @Transactional
     public void createMessage(ChannelTopic channelTopic, ChatMessageRequest request) {
-
+        System.out.println("2-3. 메시지 생성 ");
         ChatMessage chatMessage = ChatMessage.create(request);
 
+
+        System.out.println("2-4. 메시지 저장 및 전달 ");
         opsHashChatMessage.put(CHAT_MESSAGE, chatMessage.getId(), chatMessage);
         redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
     }
 
     @Transactional
     public ChatMessagesResponse showChatMessage(String roomId,Pageable pageable) {
+        System.out.println("3-2. 메시지 조회");
         List<ChatMessage> temps = opsHashChatMessage.values(CHAT_MESSAGE);
 
         List<ChatMessage> chatMessageList = new ArrayList<>();
 
+        System.out.println("3-3. 메시지 골라내기");
         for (ChatMessage temp : temps){
 
             if(temp.getRoomId().equals(roomId)){
-
                 chatMessageList.add(temp);
             }
         }
 
+        System.out.println("3-4. 메시지 페이지 네이션");
         int start = (int)pageable.getOffset();
         int end = Math.min((start+pageable.getPageSize()), chatMessageList.size());
         Page<ChatMessage> chatMessages = new PageImpl<>(chatMessageList.subList(start,end),pageable,chatMessageList.size());
 
+        System.out.println("3-4. 메시지 리스트 반환");
         return ChatMessagesResponse.from(chatMessages);
-
     }
 }
