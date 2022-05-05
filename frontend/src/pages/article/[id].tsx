4302632195fcc3db11bbcdb5f7852c3d48d02043
우동진 <wjinh93@gmail.com>
2022-05-05 16:07:28 +0900

    [S06P31D105-38] feat: 상세페이지 UI구조 구현

--- frontend/src/pages/article/[id].tsx
+++ frontend/src/pages/article/[id].tsx
@@ -1,12 +1,30 @@
+import PriceInfo from "@components/article/PriceInfo";
 import React from "react";
 import styled from "styled-components";
 
 function index() {
-	return <Container>index</Container>;
+	return (
+		<Container>
+			<SaleInfo>
+				<PriceInfo />
+			</SaleInfo>
+			<Card></Card>
+		</Container>
+	);
 }
 
 export default index;
 
 const Container = styled.div`
+	width: 120rem;
 	display: flex;
+	margin: 0 auto;
+`;
+
+const SaleInfo = styled.div`
+	width: 72rem;
+`;
+
+const Card = styled.div`
+	width: 46rem;
 `;
