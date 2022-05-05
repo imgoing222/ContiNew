221f0b15046d4eb1bb1687b2ec031518479deb71
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-05 21:03:54 +0900

    [S06P31D105-106] feat: ChatListItem 초안

--- frontend/src/components/chat/ChatListItem.tsx
+++ frontend/src/components/chat/ChatListItem.tsx
@@ -1,3 +1,16 @@
-function ChatListItem() {}
+function ChatListItem() {
+	return (
+		<div>
+			<div>
+				<div>닉네임</div>
+				<div>최근 메시지</div>
+			</div>
+			<div>
+				<div>Img</div>
+				<div>시간</div>
+			</div>
+		</div>
+	);
+}
 
 export default ChatListItem;
