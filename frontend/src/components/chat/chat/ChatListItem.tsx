47ed690a8a51c4672b4f4a77f1ef79f37a93ef58
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-06 01:43:26 +0900

    [S06P31D105-41] feat: styled-componets 추가

--- frontend/src/components/chat/chat/ChatListItem.tsx
+++ frontend/src/components/chat/chat/ChatListItem.tsx
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+
 function ChatListitem() {
 	return (
-		<div>
-			<textarea name="content" cols={20}></textarea>
-		</div>
+		<Container>
+			<Textarea name="content" cols={20} readOnly></Textarea>
+		</Container>
 	);
 }
+
+const Container = styled.div``;
+
+const Textarea = styled.textarea`
+	font-size: 2rem;
+	border: solid 1px #d3d3d3;
+	resize: none;
+	border-radius: 10px;
+`;
 
 export default ChatListitem;
