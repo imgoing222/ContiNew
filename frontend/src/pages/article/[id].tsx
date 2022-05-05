19056a6875d6dee4191f51cc6b86889fdbdb52cc
우동진 <wjinh93@gmail.com>
2022-05-05 15:58:02 +0900

    [S06P31D105-38] feat: 상세 페이지 생성

---
+++ frontend/src/pages/article/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function index() {
+	return <Container>index</Container>;
+}
+
+export default index;
+
+const Container = styled.div`
+	display: flex;
+`;
