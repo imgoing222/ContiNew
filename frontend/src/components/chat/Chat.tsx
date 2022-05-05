32e7b61688aacae7a45218ea633014ec092e3bee
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-05 20:19:37 +0900

    [S06P31D105-118] refactor: 코드 정리

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -93,7 +93,8 @@ function Chat({ sendMessage, roomId }: SendMessageProps) {
 			{roomId && (
 				<div>
 					<div>
-						{chattings.chat_message && chattings.chat_message.map((chat) => <p>{chat.content}</p>)}
+						{chattings.chat_message &&
+							chattings.chat_message.map((chat) => <p key={chat.created_at}>{chat.content}</p>)}
 					</div>
 					<form onSubmit={sendMessageHandler}>
 						<textarea
