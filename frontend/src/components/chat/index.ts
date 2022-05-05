8e74da8b9832921cb3c591997ddc1ea9228c33cd
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-06 00:01:50 +0900

    [S06P31D105-41] refactor: chat 폴더 구조 변경

--- frontend/src/components/chat/index.ts
+++ frontend/src/components/chat/index.ts
@@ -1,4 +1,4 @@
-export { default as Chat } from "./Chat";
-export { default as ChatList } from "./ChatList";
-export { default as ItemDetail } from "./ItemDetail";
-export { default as ChatListItem } from "./ChatListItem";
+export { default as Chat } from "./Chat/Chat";
+export { default as ChatList } from "./ChatList/ChatList";
+export { default as ItemDetail } from "./ItemDetail/ItemDetail";
+export { default as ChatListItem } from "./ChatList/ChatListItem";
