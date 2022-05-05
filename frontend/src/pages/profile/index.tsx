026fdba8337930ceedb9e95a5c7548193ef07040
9603100 <imgoing222@gmail.com>
2022-05-06 01:47:12 +0900

    [S06P31D105-128] feat: 프로필 페이지 렌더링 시 회원 정보 받도록 구현

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -3,22 +3,35 @@ import { Input } from "@components/account/Input";
 import { Label } from "@components/account/Label";
 import { Container } from "@components/profile/Container";
 import UserInfoEdit from "@components/profile/UserInfoEdit";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import profileApi from "src/api/profile";
 import { RootState } from "src/store";
+import { SET_USER } from "src/store/user";
+import UserInfo from "src/types/UserInfo";
 
 function Profile() {
-	const { id, login_id, username, phone_nunber, phone_auth, provider } = useSelector(
-		(state: RootState) => state.userInfo,
-	);
+	const dispatch = useDispatch();
+	const [userInfo, setUserInfo] = useState<UserInfo>();
+
+	useEffect(() => {
+		profileApi.getUserInfo().then((res) => {
+			console.log(res);
+			setUserInfo(res.data);
+			dispatch(SET_USER(res.data));
+		});
+	}, []);
 
 	return (
-		<Container>
-			<p>인증 된 회원입니다</p>
-			<Label>아이디</Label>
-			<Input disabled value={login_id} />
-			<UserInfoEdit username={username} />
-			<Button>본인인증</Button>
-		</Container>
+		userInfo && (
+			<Container>
+				<p>인증 된 회원입니다</p>
+				<Label>아이디</Label>
+				<Input disabled value={userInfo.login_id} />
+				<UserInfoEdit username={userInfo.username} />
+				<Button>본인인증</Button>
+			</Container>
+		)
 	);
 }
 
