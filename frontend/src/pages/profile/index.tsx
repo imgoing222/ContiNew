8e2a825d8601cc8fe1b9b4475c46bd4105b20deb
9603100 <imgoing222@gmail.com>
2022-05-06 01:21:23 +0900

    [S06P31D105-128] feat: useSelector로 User 정보 받기

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -1,16 +1,24 @@
 import { Button } from "@components/account/Button";
 import { Input } from "@components/account/Input";
 import { Label } from "@components/account/Label";
+import { Container } from "@components/profile/Container";
+import UserInfoEdit from "@components/profile/UserInfoEdit";
+import { useSelector } from "react-redux";
+import { RootState } from "src/store";
 
 function Profile() {
+	const { id, login_id, username, phone_nunber, phone_auth, provider } = useSelector(
+		(state: RootState) => state.userInfo,
+	);
+
 	return (
-		<>
+		<Container>
 			<p>인증 된 회원입니다</p>
 			<Label>아이디</Label>
-			<Input disabled />
-			<div>회원정보 수정</div>
+			<Input disabled value={login_id} />
+			<UserInfoEdit username={username} />
 			<Button>본인인증</Button>
-		</>
+		</Container>
 	);
 }
 
