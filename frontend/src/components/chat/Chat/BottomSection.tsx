89b805d529522ce67eb20983c88ef06dcc4a6d7d
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-06 00:07:24 +0900

    [S06P31D105-41] feat: BottomSection 초안

--- frontend/src/components/chat/Chat/BottomSection.tsx
+++ frontend/src/components/chat/Chat/BottomSection.tsx
@@ -1,3 +1,10 @@
-function BottomSection() {}
+function BottomSection() {
+  return <div>
+    <textarea />
+    <form>
+      <button>보내기</button>
+    </form>
+  </div>
+}
 
 export default BottomSection;
