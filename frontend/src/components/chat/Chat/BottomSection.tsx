909f03f0d09f78a36e03014be2d9d9365ac51957
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-06 00:09:39 +0900

    [S06P31D105-41] feat: styled-components 추가

--- frontend/src/components/chat/Chat/BottomSection.tsx
+++ frontend/src/components/chat/Chat/BottomSection.tsx
@@ -1,10 +1,22 @@
+import styled from "styled-components";
+
 function BottomSection() {
-  return <div>
-    <textarea />
-    <form>
-      <button>보내기</button>
-    </form>
-  </div>
+	return (
+		<Container>
+			<Textarea />
+			<Form>
+				<Button>보내기</Button>
+			</Form>
+		</Container>
+	);
 }
+
+const Container = styled.div``;
+
+const Textarea = styled.textarea``;
+
+const Form = styled.form``;
+
+const Button = styled.button``;
 
 export default BottomSection;
