4ad4b88f4f09616d76e7783368bbd028c8cddecd
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-05 21:06:55 +0900

    [S06P31D105-106] feat: styled-components Container 추가

--- frontend/src/components/chat/ChatListItem.tsx
+++ frontend/src/components/chat/ChatListItem.tsx
@@ -1,16 +1,34 @@
+import styled from "styled-components";
+
 function ChatListItem() {
 	return (
-		<div>
-			<div>
+		<Container>
+			<LeftSection>
 				<div>닉네임</div>
 				<div>최근 메시지</div>
-			</div>
-			<div>
+			</LeftSection>
+			<RightSection>
 				<div>Img</div>
 				<div>시간</div>
-			</div>
-		</div>
+			</RightSection>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const LeftSection = styled.div`
+	display: flex;
+  flex-direction: column;
+`;
+
+const RightSection = styled.div`
+	display: flex;
+  flex-direction: column;
+	align-items: center;
+`;
 
 export default ChatListItem;
