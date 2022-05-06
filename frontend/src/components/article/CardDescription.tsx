06f6e9e119ee85e8647fa7d00d942e17ec9e6453
우동진 <wjinh93@gmail.com>
2022-05-06 17:24:03 +0900

    [S06P31D105-39] feat: 카드 구현

---
+++ frontend/src/components/article/CardDescription.tsx
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import IconPart from "./IconPart";
+
+interface TextProp {
+	margin?: string;
+}
+
+function CardDescription() {
+	const houseInfo = {
+		address_detail: "스카이빌",
+		contract_type: "월세",
+		deposit: 10000000,
+		description: "6개월 짜리 방 내놓습니다",
+		house_id: 1,
+		house_type: "원룸",
+		images: ["string"],
+		jibun_address: "서울 동대문구 이문동 294-295",
+		maintenance_detail: "전기요금",
+		maintenance_fee: 50000,
+		monthly_rent: 500000,
+		options: [1, 2, 3],
+		period: 6,
+		phone_auth: true,
+		sale_type: "이어살기",
+		username: "채채퐁",
+		floor: 3,
+	};
+
+	return (
+		<Container>
+			<Div>
+				<SaleType>{houseInfo.sale_type}</SaleType>
+				<Text>{houseInfo.username}</Text>
+			</Div>
+			<Price>
+				{houseInfo.contract_type} {houseInfo.monthly_rent / 10000}만원
+			</Price>
+			<Text margin="true">{houseInfo.jibun_address}</Text>
+			<div>
+				<IconPart
+					content1="house_type"
+					content2="period"
+					info1={houseInfo.house_type}
+					info2={houseInfo.period}
+				/>
+				<IconPart
+					content1="floor"
+					content2="price"
+					info1={houseInfo.floor}
+					info2={`월 ${houseInfo.maintenance_fee / 10000}만원`}
+				/>
+				<Hr />
+			</div>
+			<ButtonDiv>
+				<Button>채팅 하기</Button>
+				<Button>북마크</Button>
+			</ButtonDiv>
+		</Container>
+	);
+}
+
+export default CardDescription;
+
+const Container = styled.div`
+	width: 35rem;
+	border: 1px solid ${(props) => props.theme.borderColor};
+	height: 40rem;
+	padding: 2rem;
+	position: sticky;
+	top: 10rem;
+`;
+
+const SaleType = styled.h1`
+	font-size: 3rem;
+	font-weight: bold;
+	margin-right: 3rem;
+`;
+
+const Text = styled.p<TextProp>`
+	font-size: 1.5rem;
+	margin-bottom: ${({ margin }) => margin && "4rem"};
+`;
+
+const Div = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 4rem;
+`;
+
+const Hr = styled.hr`
+	background-color: #fff;
+	border-color: #fff;
+`;
+
+const Button = styled.button`
+	width: 14rem;
+	height: 4rem;
+	border: none;
+	background-color: ${(props) => props.theme.mainColor};
+	color: #fff;
+	cursor: pointer;
+	margin-right: 1rem;
+	font-size: 1.8rem;
+	text-align: center;
+`;
+
+const ButtonDiv = styled.div`
+	width: 100%;
+	display: flex;
+	margin-top: 2rem;
+	justify-content: center;
+`;
+
+const Price = styled.p`
+	font-size: 1.8rem;
+	font-weight: bold;
+	margin-bottom: 1rem;
+`;
---
+++ frontend/src/components/article/IconPart.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import Image from "next/image";
+import { HouseInfo } from "src/types/houseInfo";
+
+interface IconProps {
+	content1: string;
+	content2: string;
+	info1: string | number;
+	info2: string | number;
+}
+function IconPart({ content1, content2, info1, info2 }: IconProps) {
+	return (
+		<Ul>
+			<Li>
+				<Image src={`/${content1}.svg`} width="40" height="40" />
+				<IconText>{info1}</IconText>
+			</Li>
+			<Li>
+				<Image src={`/${content2}.svg`} width="40" height="40" />
+				<IconText>{info2}</IconText>
+			</Li>
+		</Ul>
+	);
+}
+
+export default IconPart;
+
+const Ul = styled.ul`
+	margin: 0 auto;
+	display: flex;
+	margin-bottom: 2rem;
+`;
+
+const Li = styled.li`
+	display: flex;
+	align-items: center;
+`;
+
+const IconText = styled.p`
+	font-size: 1.5rem;
+	margin-left: 2rem;
+	width: 15rem;
+`;
