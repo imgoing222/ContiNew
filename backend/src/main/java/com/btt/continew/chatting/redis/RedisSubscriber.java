2a484eb23d19b348fbca407266d3c238f211cbb6
vtaco <woosj7129@naver.com>
2022-05-06 11:50:48 +0900

    [S06P31D105-51] bugfix: RedisSubscriber publishMessage class 변경

--- backend/src/main/java/com/btt/continew/chatting/redis/RedisSubscriber.java
+++ backend/src/main/java/com/btt/continew/chatting/redis/RedisSubscriber.java
@@ -1,5 +1,6 @@
 package com.btt.continew.chatting.redis;
 
+import com.btt.continew.chatting.controller.dto.request.ChatMessageRequest;
 import com.btt.continew.chatting.domain.ChatMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +18,11 @@ public class RedisSubscriber {
 
 
     public void sendMessage(String publishMessage) {
-        System.out.println("2-2. 레디스 구독 샌드 메시지");
+        System.out.println("4-5. 레디스 구독 처리");
         try {
-
-            ChatMessage chatMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
-
+            System.out.println("publishMessage : "+publishMessage);
+            ChatMessageRequest chatMessage = objectMapper.readValue(publishMessage, ChatMessageRequest.class);
+            System.out.println("4-6. 레디스 메시지 전달");
             messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(), chatMessage);
         } catch (Exception e) {
             log.error("Exception {}", e);
