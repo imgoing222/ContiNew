0465e009ee9d920f5868d8fd7bcae2434362aaaa
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-06 13:04:34 +0900

    [S06P310105-14] feat: 주석

--- backend/src/main/java/com/btt/continew/member/service/MemberDeleteService.java
+++ backend/src/main/java/com/btt/continew/member/service/MemberDeleteService.java
@@ -17,6 +17,13 @@ public class MemberDeleteService {
     public void resignMember(String loginId){
         Member member = memberService.findByLoginIdAndDeletedAtNull(loginId);
 
+        // 여기에 해당 서비스의 삭제 로직을 넣으시면 됩니다.
+        // 로그인 관련
+
+        // 매물 관련
+
+        // 채팅 관련
+
         member.changeUsername("resign-"+member.getId());
         member.saveDeletedTime();
     }
