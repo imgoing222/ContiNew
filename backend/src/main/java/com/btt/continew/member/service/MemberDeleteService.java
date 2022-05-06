68aadd04a9382f242d7f4dfb00e9f5db9f82c58a
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-06 12:59:06 +0900

    [S06P310105-14] feat: MemberDeleteService 에 resignMember 추가

--- backend/src/main/java/com/btt/continew/member/service/MemberDeleteService.java
+++ backend/src/main/java/com/btt/continew/member/service/MemberDeleteService.java
@@ -1,6 +1,8 @@
 package com.btt.continew.member.service;
 
+import com.btt.continew.member.domain.Member;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberDeleteService {
@@ -11,7 +13,11 @@ public class MemberDeleteService {
         this.memberService = memberService;
     }
 
+    @Transactional
     public void resignMember(String loginId){
+        Member member = memberService.findByLoginIdAndDeletedAtNull(loginId);
 
+        member.changeUsername("resign-"+member.getId());
+        member.saveDeletedTime();
     }
 }
