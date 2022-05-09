87caf994b5347732c7abc3f19c4b18db4f9dbc8a
우동진 <wjinh93@gmail.com>
2022-05-09 15:01:31 +0900

    [S06P31D105-35] feat: 구조 변경 및 photos 추가

--- frontend/src/pages/article/[id].tsx
+++ frontend/src/pages/article/[id].tsx
@@ -2,33 +2,40 @@ import CardDescription from "@components/article/CardDescription";
 import Description from "@components/article/Description";
 import LocationInfo from "@components/article/LocationInfo";
 import OptionInfo from "@components/article/OptionalInfo";
+import Photos from "@components/article/Photos";
 import PriceInfo from "@components/article/PriceInfo";
 import React from "react";
 import styled from "styled-components";
 
 function index() {
 	return (
-		<Container>
-			<SaleInfo>
-				<PriceInfo />
-				<OptionInfo />
-				<LocationInfo />
-				<Description />
-			</SaleInfo>
-			<Card>
-				<CardDescription />
-			</Card>
-		</Container>
+		<Div>
+			<Photos />
+			<Container>
+				<SaleInfo>
+					<PriceInfo />
+					<OptionInfo />
+					<LocationInfo />
+					<Description />
+				</SaleInfo>
+				<Card>
+					<CardDescription />
+				</Card>
+			</Container>
+		</Div>
 	);
 }
 
 export default index;
 
 const Container = styled.div`
-	width: 120rem;
 	display: flex;
-	margin: 0 auto;
-	justify-content: space-between;
+`;
+const Div = styled.div`
+	width: 120rem;
+	margin: 4rem auto;
+	display: flex;
+	flex-direction: column;
 `;
 
 const SaleInfo = styled.div`
