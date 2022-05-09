07aab7a4f704f705a9734bf70a9c613cf62a51b7
9603100 <imgoing222@gmail.com>
2022-05-09 23:22:02 +0900

    [S06P31D105-57] feat: 사용자 정보 입력 후 제출하는 함수 구현

--- frontend/src/pages/account/findPassword.tsx
+++ frontend/src/pages/account/findPassword.tsx
@@ -2,25 +2,53 @@ import { Button } from "@components/account/Button";
 import { Header } from "@components/account/Header";
 import { Input } from "@components/account/Input";
 import { Label } from "@components/account/Label";
-import { Container } from "@components/account/Container";
+import { Container, FormContainer } from "@components/account/Container";
+import { useState } from "react";
+import authApi from "src/api/auth";
 
 function findPassword() {
+	const [userInfo, setUserInfo] = useState({ login_id: "", phone_number: "" });
+	const [showCodeInput, setShowCodeInput] = useState(false);
+
+	const handleUserInfoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target as HTMLInputElement;
+		const tempUserInfo = { ...userInfo, [name]: value.trim() };
+		setUserInfo(tempUserInfo);
+	};
+
+	const handleUserInfoSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		try {
+			await authApi.findPassword(userInfo);
+			// 에러 토스트로 띄우기
+			setShowCodeInput(true);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
 		<Container>
-			<Header>비밀번호 찾기</Header>
-			<p>비밀번호를 잊으셨나요?</p>
-			<p>회원가입 시 입력했던 아이디와 휴대폰 번호를 입력해주시면 임시코드를 전송해드립니다.</p>
-			<Label>아이디</Label>
-			<Input />
-			<Label>휴대폰 번호</Label>
-			<Input />
-			<Button color="white" backgroundColor="#DC143C">
-				코드 전송
-			</Button>
-			<Input placeholder="코드를 입력해주세요" />
-			<Button disabled backgroundColor="dedede">
-				코드 확인
-			</Button>
+			<FormContainer onSubmit={handleUserInfoSubmit}>
+				<Header>비밀번호 찾기</Header>
+				<p>비밀번호를 잊으셨나요?</p>
+				<p>회원가입 시 입력했던 아이디와 휴대폰 번호를 입력해주시면 임시코드를 전송해드립니다.</p>
+				<Label>아이디</Label>
+				<Input name="login_id" onChange={handleUserInfoChange} />
+				<Label>휴대폰 번호</Label>
+				<Input name="phone_number" onChange={handleUserInfoChange} />
+				<Button color="white" backgroundColor="#DC143C">
+					코드 전송
+				</Button>
+			</FormContainer>
+			{showCodeInput && (
+				<div>
+					<Input placeholder="코드를 입력해주세요" />
+					<Button disabled backgroundColor="dedede">
+						코드 확인
+					</Button>
+				</div>
+			)}
 		</Container>
 	);
 }
