81761e0f994c8709ecfd1a6ecd8bc355641d2097
yeonju501 <loling970124@gmail.com>
2022-05-09 11:50:21 +0900

    [S06P31D105-133] feat: HouseRepository existsByMember 추가

--- backend/src/main/java/com/btt/continew/house/domain/HouseRepository.java
+++ backend/src/main/java/com/btt/continew/house/domain/HouseRepository.java
@@ -1,5 +1,6 @@
 package com.btt.continew.house.domain;
 
+import com.btt.continew.member.domain.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ public interface HouseRepository extends JpaRepository<House, Long> {
 
     Page<House> findAllByLatitudeBetweenAndLongitudeBetween(Long latStart, Long latEnd, Long lngStart, Long lngEnd,
         Pageable pageable);
+
+    boolean existsByMember(Member member);
 }
