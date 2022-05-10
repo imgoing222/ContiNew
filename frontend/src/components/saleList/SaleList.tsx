296a185a395893dff19330efc0f95ed47a824a4e
vtaco <woosj7129@naver.com>
2022-05-11 00:17:38 +0900

    [S06P31D105-82] deploy: 폴더명 소문자로 변경

---
+++ frontend/src/components/saleList/SaleList.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function SaleList() {
+	return <Container></Container>;
+}
+
+export default SaleList;
+
+const Container = styled.section`
+	min-width: 38rem;
+	border: 1px solid #000;
+	display: flex;
+	flex-direction: column;
+`;
