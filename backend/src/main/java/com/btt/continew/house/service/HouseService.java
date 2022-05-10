8c878c2df324ac223e3dc9ca327300f0bef1b7ee
yeonju501 <loling970124@gmail.com>
2022-05-10 13:18:00 +0900

    [S06P31D105-152] feat: HouseService options 자료형 변경

--- backend/src/main/java/com/btt/continew/house/service/HouseService.java
+++ backend/src/main/java/com/btt/continew/house/service/HouseService.java
@@ -81,7 +81,7 @@ public class HouseService {
             .contractType(request.getContractType())
             .period(request.getPeriod())
             .expiredAt(LocalDateTime.now().plusMonths(LISTING_PERIOD))
-            .options(request.getOptions().substring(1, request.getOptions().length()-1))
+            .options(request.getOptions().toString().substring(1, request.getOptions().toString().length()-1))
             .build();
         houseRepository.save(house);
 
