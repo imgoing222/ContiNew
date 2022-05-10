e74dac9b131e8499267fbb0c909657c8fc2aeb12
yeonju501 <loling970124@gmail.com>
2022-05-10 17:10:14 +0900

    [S06P31D105-112] feat: HouseLikeRepository findByHouseAndMember 추가

--- backend/src/main/java/com/btt/continew/house/domain/HouseLikeRepository.java
+++ backend/src/main/java/com/btt/continew/house/domain/HouseLikeRepository.java
@@ -1,9 +1,12 @@
 package com.btt.continew.house.domain;
 
 import com.btt.continew.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HouseLikeRepository extends JpaRepository<HouseLike, Long> {
 
     boolean existsByHouseAndMember(House house, Member member);
+
+    Optional<HouseLike> findByHouseAndMember(House house, Member member);
 }
