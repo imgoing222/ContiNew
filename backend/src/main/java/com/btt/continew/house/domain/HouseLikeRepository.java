862747201a94f5fe7ff590b9c3db03e409e41ada
yeonju501 <loling970124@gmail.com>
2022-05-11 01:14:39 +0900

    [S06P31D105-156] feat: HouseLikeRepository findAllByMember 추가

--- backend/src/main/java/com/btt/continew/house/domain/HouseLikeRepository.java
+++ backend/src/main/java/com/btt/continew/house/domain/HouseLikeRepository.java
@@ -2,6 +2,8 @@ package com.btt.continew.house.domain;
 
 import com.btt.continew.member.domain.Member;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HouseLikeRepository extends JpaRepository<HouseLike, Long> {
@@ -9,4 +11,6 @@ public interface HouseLikeRepository extends JpaRepository<HouseLike, Long> {
     boolean existsByHouseAndMember(House house, Member member);
 
     Optional<HouseLike> findByHouseAndMember(House house, Member member);
+
+    Page<HouseLike> findAllByMember(Member member, Pageable pageable);
 }
