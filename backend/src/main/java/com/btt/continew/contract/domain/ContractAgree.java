07ff6962843d96c3f7321fc49327e669732411ab
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-10 11:14:33 +0900

    [S06P31D105-145] feat: ContractAgree 에 house_id 추가

--- backend/src/main/java/com/btt/continew/contract/domain/ContractAgree.java
+++ backend/src/main/java/com/btt/continew/contract/domain/ContractAgree.java
@@ -1,6 +1,7 @@
 package com.btt.continew.contract.domain;
 
 import com.btt.continew.global.domain.BaseEntity;
+import com.btt.continew.house.domain.House;
 import com.btt.continew.member.domain.Member;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -24,6 +25,10 @@ public class ContractAgree extends BaseEntity {
     @Column(name = "contract_agree_id")
     private Long id;
 
+    @ManyToOne(targetEntity = House.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "house_id", nullable = false)
+    private House house;
+
     @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "seller_id", nullable = false)
     private Member seller;
@@ -42,7 +47,8 @@ public class ContractAgree extends BaseEntity {
     }
 
     @Builder
-    public ContractAgree(Member seller, Member buyer, Boolean sellerAgree, Boolean buyerAgree) {
+    public ContractAgree(House house, Member seller, Member buyer, Boolean sellerAgree, Boolean buyerAgree) {
+        this.house = house;
         this.seller = seller;
         this.buyer = buyer;
         this.sellerAgree = sellerAgree;
