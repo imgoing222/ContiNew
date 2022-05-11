4161f44e7ea78df89a01b86ba5acbb63436be42c
우동진 <wjinh93@gmail.com>
2022-05-11 16:01:04 +0900

    [S06P31D105-78] feat: 리스트 컴포넌트 구현

--- frontend/src/components/saleList/SaleList.tsx
+++ frontend/src/components/saleList/SaleList.tsx
@@ -1,15 +1,44 @@
 import House from "src/types/getListType";
 import styled from "styled-components";
+import { moneyUnitChange } from "@utils/index";
+import Link from "next/link";
 
 interface SaleListProps {
 	saleList: House[];
+}
+
+interface TextProp {
+	fontSize?: number;
+	bold?: boolean;
+	color?: string;
 }
 
 function SaleList({ saleList }: SaleListProps) {
 	return (
 		<Container>
 			{saleList.map((item, idx) => (
-				<p key={idx}>{item.address_detail}</p>
+				<Link href={{ pathname: `/article/${item.house_id}`, query: { id: item.house_id } }}>
+					<Box key={idx}>
+						<MainImg src={item.main_image}></MainImg>
+						<TextBox>
+							<Text fontSize={1.6} bold={true}>
+								{item.contractType} {moneyUnitChange(item.deposit.toString())}
+								{item.contractType === "월세" &&
+									"/ " + moneyUnitChange(item.monthly_rent.toString())}
+							</Text>
+							<Text fontSize={1.2}>{item.house_type}</Text>
+							<Text fontSize={1.2}>
+								{item.sido_name} {item.gungu_name} {item.dong_name}
+							</Text>
+							<Text fontSize={1.2}>관리비 10만 3층</Text>
+							<Text fontSize={1.2}>
+								{item.description.length > 24
+									? item.description.slice(0, 24) + "..."
+									: item.description}
+							</Text>
+						</TextBox>
+					</Box>
+				</Link>
 			))}
 		</Container>
 	);
@@ -19,7 +48,30 @@ export default SaleList;
 
 const Container = styled.section`
 	min-width: 38rem;
-	border: 1px solid #000;
 	display: flex;
 	flex-direction: column;
+	margin-top: 1rem;
+`;
+
+const Box = styled.div`
+	display: flex;
+	margin-bottom: 1.5rem;
+	cursor: pointer;
+`;
+
+const MainImg = styled.img`
+	width: 12rem;
+	height: 13rem;
+	margin-right: 1rem;
+`;
+
+const TextBox = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Text = styled.p<TextProp>`
+	font-size: ${({ fontSize }) => (fontSize ? +fontSize + "rem" : "1.3rem")};
+	font-weight: ${({ bold }) => bold && "bold"};
+	margin-bottom: 0.5rem;
 `;
