e9dc0d125163790d83f9a26703476561f17da600
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-11 23:23:48 +0900

    [S06P31D105-138] feat: 주변 매물 조회 api 함수 생성

--- frontend/src/api/main.ts
+++ frontend/src/api/main.ts
@@ -1,8 +1,18 @@
 import { AxiosResponse } from "axios";
 import { request } from "./request";
 
-interface MainApiType {}
+interface MainApiType {
+	getAroundHouse: (addressData: AddressDataType) => Promise<AxiosResponse>;
+}
 
-const mainApi: MainApiType = {};
+interface AddressDataType {
+	sido_name: string;
+	gungu_name: string;
+	dong_name: string;
+}
+
+const mainApi: MainApiType = {
+  getAroundHouse: (addressData) => request.post("houses/around", addressData),
+};
 
 export default mainApi;
