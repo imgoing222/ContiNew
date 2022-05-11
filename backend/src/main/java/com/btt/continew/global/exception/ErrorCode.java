a20b750e9aea1cfd6510fb139474dd31992fae19
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-11 12:46:43 +0900

    [S06P31D105-149] feat: 계약서 Null 대응 ErrorCode 추가

--- backend/src/main/java/com/btt/continew/global/exception/ErrorCode.java
+++ backend/src/main/java/com/btt/continew/global/exception/ErrorCode.java
@@ -64,6 +64,7 @@ public enum ErrorCode {
     CONTRACT_NOT_FOUND_CONTRACT(HttpStatus.NOT_FOUND, "계약서를 찾을 수 없습니다.", "K08"),
     CONTRACT_ALREADY_FINISHED(HttpStatus.BAD_REQUEST, "이미 작성이 완료된 계약서 입니다.", "K09"),
     CONTRACT_WEIRD_LEVEL(HttpStatus.CONFLICT, "존재하지 않는 단계입니다.", "K10"),
+    CONTRACT_NULL_DATA(HttpStatus.BAD_REQUEST, "빈공간이 존재합니다.", "K11"),
 
     // house
     HOUSE_NOT_FOUND_BY_ID(HttpStatus.NOT_FOUND, "존재하지 않는 매물입니다", "H01"),
