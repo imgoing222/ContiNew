8d35ff1aeec5a7475a32089bec1a9b0e73601742
9603100 <imgoing222@gmail.com>
2022-05-11 12:21:50 +0900

    [S06P31D105-159] feat: 비밀번호 변경 함수 구현

--- frontend/src/pages/account/changePassword.tsx
+++ frontend/src/pages/account/changePassword.tsx
@@ -2,15 +2,31 @@ import { Button } from "@components/account/Button";
 import { Container } from "@components/account/Container";
 import { Header } from "@components/account/Header";
 import { Input } from "@components/account/Input";
-import { Label } from "@components/account/Label";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import authApi from "src/api/auth";
 
 function ChangePassword() {
+	const router = useRouter();
+	const changeToken = router.query.name as string;
+	const [value, setValue] = useState({ new_password: "", change_token: changeToken });
+
+	const handleNewPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue({ ...value, new_password: e.target.value });
+	};
+
+	const handleChangeClick = async () => {
+		await authApi.changePassword(value);
+		alert("비밀번호가 변경되었습니다.");
+		router.push("account/signin");
+	};
+
 	return (
 		<Container>
 			<Header>비밀번호 변경</Header>
-			<Label>새로운 비밀번호</Label>
-			<Input />
-			<Button>변경</Button>
+			<p>새로운 비밀번호를 입력하세요</p>
+			<Input onChange={handleNewPasswordChange} />
+			<Button onClick={handleChangeClick}>변경</Button>
 		</Container>
 	);
 }
