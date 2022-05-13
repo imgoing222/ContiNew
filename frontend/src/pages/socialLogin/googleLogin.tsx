5c7d484b6bfa665b08aad3b941a56058e1e6aa05
9603100 <imgoing222@gmail.com>
2022-05-13 14:04:43 +0900

    [S06P31D105-182] refactor: 구글 로그인 수정

--- frontend/src/pages/socialLogin/googleLogin.tsx
+++ frontend/src/pages/socialLogin/googleLogin.tsx
@@ -1,11 +1,14 @@
 import { useRouter } from "next/router";
 import authApi from "src/api/auth";
+import cookie from "react-cookies";
 
 function GoogleLogin() {
 	const router = useRouter();
 	const code = encodeURIComponent(router.query.code as string);
-	// window.location.replace(`http://localhost:8080/api/members/login/google/callback?code=${code}`);
-	// location.replace("/");
+	authApi.googleLogin(code).then(() => {
+		console.log("success");
+		if (cookie.load("access_token")) location.replace("/");
+	});
 	return;
 }
 
