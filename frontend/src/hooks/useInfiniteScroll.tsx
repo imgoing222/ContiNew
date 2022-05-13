3a0349e523a8c096ed0793b07f7c96b9987b12b7
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-13 09:43:04 +0900

    [S06P31D105-136] feat: getChatList 함수 추가

--- frontend/src/hooks/useInfiniteScroll.tsx
+++ frontend/src/hooks/useInfiniteScroll.tsx
@@ -1,3 +1,41 @@
-const useInfiniteScroll = () => {};
+import { useEffect, useState } from "react";
+import useIntersectionObserver from "./useIntersectionObserver";
+
+interface useInfiniteScrollProps {
+	requestApi: (currentPage: number) => Promise<any>;
+}
+
+interface ChatMessageType {
+	room_id: string;
+	sender: string;
+	content: string;
+	read_at: string;
+	created_at: string;
+}
+
+const useInfiniteScroll = ({ requestApi }: useInfiniteScrollProps) => {
+	const [savedChatMessage, setSavedChatMessage] = useState<ChatMessageType[]>([]);
+	const [totalPage, setTotalPage] = useState(0);
+	const [currentPage, setCurrentPage] = useState(0);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const getChatList = async (currentPage: number) => {
+		try {
+			setIsLoading(true);
+			await new Promise((resolve) => setTimeout(resolve, 500));
+
+			const res = await requestApi(currentPage);
+			setTotalPage(res.data.total_page_count);
+			setSavedChatMessage((prevSavedChatMessage) => [
+				...prevSavedChatMessage,
+				...res.data.chat_message,
+			]);
+
+			setIsLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+};
 
 export default useInfiniteScroll;
