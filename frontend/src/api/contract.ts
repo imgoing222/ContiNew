530387211b1e9123a8af1a3f7f3fa1e666eae676
9603100 <imgoing222@gmail.com>
2022-05-14 00:06:14 +0900

    [S06P31D105-173] feat: 계약서 조회 api 요청 함수 작성

--- frontend/src/api/contract.ts
+++ frontend/src/api/contract.ts
@@ -2,10 +2,13 @@ import { AxiosResponse } from "axios";
 import { request } from "./request";
 
 interface ContractApiType {
+	getContract: (buyer: string, house_id: number, seller: string) => Promise<AxiosResponse>;
 	breakContract: (buyer: string, house_id: number, seller: string) => Promise<AxiosResponse>;
 }
 
 const contractApi: ContractApiType = {
+	getContract: (buyer, house_id, seller) =>
+		request.get(`auth/contracts?buyer=${buyer}&house_id=${house_id}&seller=${seller}`),
 	breakContract: (buyer, house_id, seller) =>
 		request.delete(`auth/contracts?buyer=${buyer}&house_id=${house_id}&seller=${seller}`),
 };
