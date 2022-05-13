0292c3dc7bfd3d7403129c9197b9d213bd95cae8
yeonju501 <loling970124@gmail.com>
2022-05-13 23:44:09 +0900

    [S06P31D105-187] feat: AuthorizationKakao 추가

---
+++ backend/src/main/java/com/btt/continew/auth/controller/dto/AuthorizationKakao.java
@@ -0,0 +1,14 @@
+package com.btt.continew.auth.controller.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthorizationKakao {
+
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private String expires_in;
+    private String scope;
+    private String refresh_token_expires_in;
+}
