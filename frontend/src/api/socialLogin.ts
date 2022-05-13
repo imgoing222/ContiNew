a33116a954405ca371c2eb3f05b08c80efc9f4ee
9603100 <imgoing222@gmail.com>
2022-05-14 00:40:56 +0900

    [S06P31D105-188] feat: 카카오 로그인 함수 파일 생성

---
+++ frontend/src/api/socialLogin.ts
@@ -0,0 +1,13 @@
+import { request } from "./request";
+
+const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_LOGIN_CLIENT_ID;
+const KAKAO_URI = process.env.NEXT_PUBLIC_KAKAO_URL;
+
+const socialLogin = {
+	kakao: () =>
+		(window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_URI}&response_type=code`),
+	kakaoLogin: (code: string) =>
+		request.get(`oauth2/authorization/kakao?code=${code}`).then((res) => console.log(res)),
+};
+
+export default socialLogin;
