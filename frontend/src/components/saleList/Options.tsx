e7f7a9c164ac917c2b44067ed3431af810b10332
우동진 <wjinh93@gmail.com>
2022-05-14 20:23:07 +0900

    [S06P31D015-32] feat: options 필터 구현

--- frontend/src/components/saleList/Options.tsx
+++ frontend/src/components/saleList/Options.tsx
@@ -1,13 +1,25 @@
-import { OptionInfo } from "@components/createSale";
 import OptionInfoList from "@components/createSale/OptionInfoList";
 import React, { useState } from "react";
-import HouseInfo from "src/types/houseInfo";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "src/store";
+import { setOptions } from "src/store/searchFilter";
 import styled from "styled-components";
 import Container from "./Container";
 
 function Options() {
-	const [a, setA] = useState();
-	const changeEvent = (e: React.ChangeEvent<HTMLInputElement>) => {};
+	const dispatch = useDispatch();
+	const houseInfo = useSelector((state: RootState) => state.searchFilter);
+	const optionsList = houseInfo["options"] as number[];
+	const changeEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const idx = optionsList.indexOf(+e.target.value);
+		if (idx !== -1) {
+			const deleteOption = optionsList.slice().filter((i) => i !== +e.target.value);
+			return dispatch(setOptions({ options: deleteOption }));
+		}
+		const addOptions = optionsList.slice();
+		addOptions.push(+e.target.value);
+		dispatch(setOptions({ options: addOptions }));
+	};
 	const options = [
 		"에어컨",
 		"세탁기",
@@ -24,7 +36,12 @@ function Options() {
 			<StyledUl>
 				{options.map((option, idx) => (
 					<Div key={idx}>
-						<OptionInfoList value={idx + 1} title={option} changeEvent={changeEvent} />
+						<OptionInfoList
+							value={idx + 1}
+							title={option}
+							changeEvent={changeEvent}
+							houseInfo={houseInfo}
+						/>
 					</Div>
 				))}
 			</StyledUl>
