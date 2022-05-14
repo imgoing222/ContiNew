45619153cdce7a777ad6606064caf45670cf48fc
우동진 <wjinh93@gmail.com>
2022-05-14 18:26:49 +0900

    [S06P31D105-32] feat: 함수 구현

--- frontend/src/components/saleList/Slider.tsx
+++ frontend/src/components/saleList/Slider.tsx
@@ -2,14 +2,19 @@ import React, { InputHTMLAttributes, useState } from "react";
 import styled from "styled-components";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
-import { moneyUnitChange } from "@utils/index";
+import { useDispatch, useSelector } from "react-redux";
+import { actionProps } from "src/store/searchFilter";
+import { RootState } from "src/store";
 
 interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
 	title?: string;
-	step: number;
 	maxMin: { min: number; max: number };
 	subTitle?: string;
-	unit: string;
+	setChange: (data: actionProps) => {
+		type: string;
+		data: actionProps;
+	};
+	itemName?: string;
 }
 
 interface RangeValue {
@@ -21,14 +26,40 @@ interface RangeProps {
 	color?: string;
 }
 
-function Slider({ title, subTitle, step, maxMin, unit }: Props) {
-	const [localMin, setLocalMin] = useState(maxMin.min);
-	const [localMax, setLocalMax] = useState(maxMin.max);
+function Slider({ title, subTitle, maxMin, setChange, itemName }: Props) {
+	const searchCondition = useSelector((state: RootState) => state.searchFilter);
+	const dispatch = useDispatch();
+	const [localMin, setLocalMin] = useState(searchCondition[`min${itemName}`] as number);
+	const [localMax, setLocalMax] = useState(searchCondition[`max${itemName}`] as number);
 
 	const onChangeHandler = (value: RangeValue) => {
 		const { min, max } = value;
 		setLocalMin(Math.max(min, maxMin.min));
 		setLocalMax(Math.min(max, maxMin.max));
+	};
+
+	const changeReduxHandler = () => {
+		dispatch(setChange({ min: localMin, max: localMax }));
+	};
+
+	const changeMoneyRange = (money: number) => {
+		if (money === 0) return 0;
+		if (subTitle === "월세" && money === 300) return "무제한";
+		if (subTitle === "보증금 / 전세금" && money === 10000) return "무제한";
+		if (subTitle === "관리비" && money === 50) return "무제한";
+		return `${money} 만원`;
+	};
+
+	const setStep = () => {
+		if (subTitle === "보증금 / 전세금") {
+			if (localMin <= 500 || localMax <= 500) return 100;
+			return 500;
+		}
+		if (subTitle === "월세") return 5;
+		if (subTitle === "관리비") {
+			if (localMin <= 30) return 1;
+			return 2;
+		}
 	};
 
 	return (
@@ -39,7 +70,7 @@ function Slider({ title, subTitle, step, maxMin, unit }: Props) {
 				<Range>{subTitle}</Range>
 
 				<Range color="red">
-					{moneyUnitChange(localMin.toString())} ~ {moneyUnitChange(localMax.toString())}
+					{changeMoneyRange(localMin)} ~ {changeMoneyRange(localMax)}
 				</Range>
 			</Description>
 
@@ -49,12 +80,12 @@ function Slider({ title, subTitle, step, maxMin, unit }: Props) {
 					formatLabel={(value) => `${value}`}
 					maxValue={maxMin.max}
 					minValue={maxMin.min}
-					step={localMax > 3000 ? step * 2 : step}
+					step={setStep()}
 					value={{ min: localMin, max: localMax }}
 					onChange={(value) => {
 						onChangeHandler(value as RangeValue);
 					}}
-					onChangeComplete={(value) => console.log(value)}
+					onChangeComplete={() => changeReduxHandler()}
 				/>
 			</Form>
 		</>
