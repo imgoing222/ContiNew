afe9f14388f2025d00adb70b00e0be8e09711d11
우동진 <wjinh93@gmail.com>
2022-05-14 23:49:20 +0900

    [S06P31D105-34] feat: 사진 부분 변경

--- frontend/src/components/article/Photos.tsx
+++ frontend/src/components/article/Photos.tsx
@@ -1,37 +1,80 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
 import styled from "styled-components";
-import "@splidejs/splide/dist/css/splide.min.css";
 import { HouseInfoProps } from "src/pages/article/[id]";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 function Photos({ houseInfo }: HouseInfoProps) {
+	const [screenSize, setScreenSize] = useState(1);
+
+	window.onresize = function () {
+		setScreenSize(window.outerWidth);
+		screenSize;
+	};
+
+	const handleScroll = (direction?: string) => {
+		if (direction) return document.getElementById("container")?.scrollBy(-250, 0);
+		document.getElementById("container")?.scrollBy(250, 0);
+	};
+
 	return (
-		<Container
-			aria-label="House Images"
-			options={{
-				width: 1200,
-				height: 300,
-				drag: true,
-				rewind: true,
-			}}
-		>
-			{houseInfo.images.map((item, idx) => (
-				<SplideSlide key={idx}>
-					<Img src={item} />
-				</SplideSlide>
-			))}
+		<Container>
+			<StyledDiv id="container">
+				<ButtonDiv direction="right">
+					<Button icon={faChevronRight} onClick={() => handleScroll()} />
+				</ButtonDiv>
+				<ButtonDiv>
+					<Button icon={faChevronLeft} onClick={() => handleScroll("left")} />
+				</ButtonDiv>
+				{houseInfo.images.map((item, idx) => (
+					<Img src={item} key={idx} />
+				))}
+			</StyledDiv>
 		</Container>
 	);
 }
 
 export default Photos;
 
-const Container = styled(Splide)`
-	display: flex;
+const Container = styled.div`
 	margin-bottom: 4rem;
-	justify-content: center;
+	position: relative;
 `;
 
 const Img = styled.img`
-	width: 100%;
-	height: 100%;
+	width: auto;
+	max-height: 30rem;
+	margin-right: 2rem;
+`;
+
+const StyledDiv = styled.div`
+	display: flex;
+	overflow-x: scroll;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
+
+interface Button {
+	direction?: string;
+}
+const Button = styled(FontAwesomeIcon)`
+	display: block;
+	font-size: 3rem;
+	z-index: 2;
+	cursor: pointer;
+	color: #fff;
+`;
+
+const ButtonDiv = styled.div<Button>`
+	width: 5rem;
+	height: 5rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: rgba(34, 34, 34, 0.6);
+	top: 45%;
+	position: absolute;
+	right: ${({ direction }) => direction && "-5rem"};
+	left: ${({ direction }) => !direction && "-5rem"};
 `;
