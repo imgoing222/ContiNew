26d3e9b6f4f6db5d0648dae68e2e7038852aab17
9603100 <imgoing222@gmail.com>
2022-05-16 03:01:20 +0900

    [S06P31D105-174] feat: 서명 제외 1, 2단계별 계약서 폼 조건부 렌더링

--- frontend/src/components/contract/SellerInfo.tsx
+++ frontend/src/components/contract/SellerInfo.tsx
@@ -1,18 +1,33 @@
+import { useEffect, useState } from "react";
+import { ContractType } from "src/types/contractType";
 import { Input } from "./Input";
 import { Label } from "./Label";
 import Signature from "./Signature";
+interface Props {
+	step: number;
+	role: string;
+	contractInfo: ContractType;
+}
 
-function SellerInfo() {
+function SellerInfo({ step, role, contractInfo }: Props) {
+	const [disabled, setDisabled] = useState(true);
+
+	useEffect(() => {
+		if (step === 1 && role === "seller") {
+			setDisabled(false);
+		}
+	}, []);
+
 	return (
 		<>
 			<Label>임차인 주소</Label>
-			<Input />
+			<Input disabled={disabled} />
 			<Label>임차인 성명</Label>
-			<Input />
+			<Input disabled={disabled} />
 			<Label>임차인 생년월일</Label>
-			<Input />
+			<Input disabled={disabled} />
 			<Label>임차인 전화</Label>
-			<Input />
+			<Input disabled={disabled} />
 			<Label>서명</Label>
 			<Signature />
 		</>
--- frontend/src/pages/contract/[id].tsx
+++ frontend/src/pages/contract/[id].tsx
@@ -1,39 +1,61 @@
 import ContractForm from "@components/contract/ContractForm";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import contractApi from "src/api/contract";
-import ContractType from "src/types/contractType";
+import { RootState } from "src/store";
+import { SET_CONTRACT, SET_ROLE } from "src/store/contract";
+import { ContractType } from "src/types/contractType";
 
 function Contract() {
 	// 매물 id로 url 생성
-	// 매물id, seller_id, buyer_id 리덕스에서 받아오기
+	// 매물id, seller_id, buyer_id 계약 버튼 눌렀을때 / 내 계약에서 눌렀을 때 받아올것 router.query로
+
+	// 더미데이터
+	const buyer = "test1212";
+	const seller = "wjinh";
+	const houseId = 43;
 
 	const router = useRouter();
-	const [contractInfo, setContractInfo] = useState<ContractType>();
+	const dispatch = useDispatch();
+	const contract = useSelector((state: RootState) => state.contractInfo);
+	const loginId = useSelector((state: RootState) => state.userInfo.login_id);
+	console.log(contract);
+	const step = contract["step"]["current_step"];
+
+	const value = { buyer, seller, house_id: houseId };
 
 	useEffect(() => {
 		getContractInfo();
+		console.log(loginId);
+		if (loginId === buyer) dispatch(SET_ROLE("buyer"));
+		else dispatch(SET_ROLE("seller"));
 	}, []);
 
 	const getContractInfo = async () => {
-		// const res = await contractApi.getContract(buyer, houseId, seller);
-		// setContractInfo(res.data);
+		const res = await contractApi.getContract(value);
+		console.log(res);
+		if (res.status) {
+			dispatch(SET_CONTRACT(res.data));
+		}
 	};
 
 	const handleBreakContractButton = async () => {
 		if (window.confirm("계약을 파기하시겠습니까?")) {
-			// await contractApi.breakContract(buyer, houseId, seller);
+			await contractApi.breakContract(value);
 			router.push("/");
 		}
 	};
 
+	const handleNextStepClick = () => {};
+
 	return (
 		<>
-			// 계약 정보 계약 폼에 전달, 계약 단계는 따로 step이라는 props로 전달
+			<h1>{step}단계</h1>
 			<ContractForm />
 			<div>
 				<button>임시 저장</button>
-				<button>다음 단계</button>
+				<button onClick={handleNextStepClick}>다음 단계</button>
 			</div>
 			<button onClick={handleBreakContractButton}>계약 파기</button>
 		</>
