2e8e1eb23e1adc455ef021e49e2239eda60da1b8
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-15 21:47:14 +0900

    [S06P31D105-191] feat: RecommendItem 초안 생성

--- frontend/src/components/main/RecommendItem.tsx
+++ frontend/src/components/main/RecommendItem.tsx
@@ -1,3 +1,26 @@
-function RecommendItem() {}
+import styled from "styled-components";
+
+import House from "src/types/getListType";
+
+interface HouseType {
+	house: House;
+}
+
+function RecommendItem({ house }: HouseType) {
+	return (
+		<Li>
+			<Image src={house.main_image} alt="house-img" />
+		</Li>
+	);
+}
+
+const Li = styled.li`
+	margin: 1rem;
+`;
+
+const Image = styled.img`
+	width: 15rem;
+	height: 15rem;
+`;
 
 export default RecommendItem;
