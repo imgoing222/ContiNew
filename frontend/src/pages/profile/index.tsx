25c6cfb0e316cbaa94dc139600e1ba39840b2f57
9603100 <imgoing222@gmail.com>
2022-05-16 14:51:58 +0900

    [S06P31D105-179] refactor: 탭 색상 임시 변경

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -1,5 +1,5 @@
 import { Container } from "@components/profile/Container";
-import Favorites from "@components/profile/Favorites";
+import Favorites from "@components/profile/Bookmarks";
 import Tabs from "@components/profile/Tabs";
 import UserInfo from "@components/profile/UserInfo";
 import { useState } from "react";
@@ -8,7 +8,7 @@ function Profile() {
 	const [onUserInfo, setOnUserInfo] = useState(true);
 	return (
 		<Container>
-			<Tabs setOnUserInfo={setOnUserInfo} />
+			<Tabs onUserInfo={onUserInfo} setOnUserInfo={setOnUserInfo} />
 			{onUserInfo ? <UserInfo /> : <Favorites />}
 		</Container>
 	);
