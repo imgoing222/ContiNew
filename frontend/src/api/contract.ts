1e994c65dcb66677ec9c00cd337d869ce67ea4d2
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-16 22:17:27 +0900

    [S06P31D105-192] feat: getContractAgree api 추가

--- frontend/src/api/contract.ts
+++ frontend/src/api/contract.ts
@@ -4,13 +4,16 @@ import { ContractType } from "src/types/contractType";
 
 interface required {
 	buyer: string;
+	buyer_agree: string;
 	house_id: number;
 	seller: string;
+	seller_agree: string;
 }
 interface ContractApiType {
 	getContract: (value: required) => Promise<AxiosResponse>;
 	breakContract: (value: required) => Promise<AxiosResponse>;
 	createContract: (contractInfo: ContractType) => Promise<AxiosResponse>;
+	getContractAgree: (vlalue: required) => Promise<AxiosResponse>;
 }
 
 const contractApi: ContractApiType = {
@@ -19,6 +22,10 @@ const contractApi: ContractApiType = {
 	breakContract: ({ buyer, seller, house_id }) =>
 		request.delete(`auth/contracts?buyer=${buyer}&house_id=${house_id}&seller=${seller}`),
 	createContract: (contractInfo) => request.post(`auth/contracts`, contractInfo),
+	getContractAgree: ({ buyer_agree, house_id, seller_agree }) =>
+		request.get(
+			`auth/contracts/agree?buyer_agree=${buyer_agree}&house_id=${house_id}&seller_agree=${seller_agree}`,
+		),
 };
 
 export default contractApi;
