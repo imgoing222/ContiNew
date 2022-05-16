bbd2865c01aed0473817c1d6e47dc8b63ab65f57
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-16 22:24:53 +0900

    S06P31D105-192] feat: agreeContract api 추가

--- frontend/src/api/contract.ts
+++ frontend/src/api/contract.ts
@@ -9,11 +9,18 @@ interface required {
 	seller: string;
 	seller_agree: string;
 }
+interface ContractRequestType {
+	house_id: number;
+	seller_login_id: string;
+	buyer_login_id: string;
+	member_type: string;
+}
 interface ContractApiType {
 	getContract: (value: required) => Promise<AxiosResponse>;
 	breakContract: (value: required) => Promise<AxiosResponse>;
 	createContract: (contractInfo: ContractType) => Promise<AxiosResponse>;
 	getContractAgree: (vlalue: required) => Promise<AxiosResponse>;
+	agreeContract: (requestInfo: ContractRequestType) => Promise<AxiosResponse>;
 }
 
 const contractApi: ContractApiType = {
@@ -26,6 +33,7 @@ const contractApi: ContractApiType = {
 		request.get(
 			`auth/contracts/agree?buyer_agree=${buyer_agree}&house_id=${house_id}&seller_agree=${seller_agree}`,
 		),
+	agreeContract: (requestInfo) => request.post(`auth/contracts`, requestInfo),
 };
 
 export default contractApi;
