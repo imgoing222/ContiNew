637968887e4583e56e0409b942778e545d62cf24
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-16 23:04:47 +0900

    S06P31D105-192] feat: getContractRequest api 적용

--- frontend/src/components/chat/itemDetail/ContractButton.tsx
+++ frontend/src/components/chat/itemDetail/ContractButton.tsx
@@ -5,10 +5,40 @@ import { RootState } from "src/store";
 
 import { contractApi } from "src/api";
 
-function ContractButton() {
-  const { buyerId, sellerId } = useSelector((state: RootState) => state.articleInfo);
+interface AgreeInfoType {
+	buyer_agree: boolean;
+	house_id: number;
+	seller_agree: boolean;
+}
 
-  console.log("buyerId : " + buyerId + "sellerId : " + sellerId );
+function ContractButton() {
+	const [isUnderContract, setIsUnderContract] = useState(false);
+	const [agreeInfo, setAgreeInfo] = useState<AgreeInfoType>({
+		buyer_agree: false,
+		house_id: 0,
+		seller_agree: false,
+	});
+	const { buyerId, sellerId, articleId } = useSelector((state: RootState) => state.articleInfo);
+
+	useEffect(() => {
+		getContractRequest();
+	}, []);
+
+	const getContractRequest = async () => {
+		try {
+			const contractRequestData = {
+				buyer: buyerId,
+				house_id: articleId,
+				seller: sellerId,
+			};
+			const res = await contractApi.getContractRequest(contractRequestData);
+			if (res.data) {
+        setAgreeInfo(res.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return <></>;
 }
 
