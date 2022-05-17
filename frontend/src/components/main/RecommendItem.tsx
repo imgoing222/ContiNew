a5042858f7598b7a9b7d9cf79755001eb99dc2dd
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-17 15:43:43 +0900

    [S06P31D105-69] style: 추천 매물 Content 추가

--- frontend/src/components/main/RecommendItem.tsx
+++ frontend/src/components/main/RecommendItem.tsx
@@ -13,6 +13,14 @@ function RecommendItem({ house }: HouseType) {
 	return (
 		<Li onClick={() => router.push(`/article/${house.house_id}`)}>
 			<Image src={house.main_image} alt="house-img" />
+			<Content>
+				<SaleType>
+					{house.contract_type} {house.deposit}
+					{house.monthly_rent && <span>/{house.monthly_rent}</span>}
+				</SaleType>
+				<Fee>관리비 : {house.maintenance_fee} 만 원</Fee>
+				<Text>{house.description}</Text>
+			</Content>
 		</Li>
 	);
 }
@@ -25,6 +33,27 @@ const Li = styled.li`
 const Image = styled.img`
 	width: 280px;
 	height: 180px;
+`;
+
+const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const SaleType = styled.h1`
+	font-size: 2.5rem;
+	font-weight: bold;
+`;
+
+const Fee = styled.h3`
+	padding-bottom: 1rem;
+	font-size: 1.5rem;
+`;
+
+const Text = styled.p`
+	padding-bottom: 1rem;
+	font-size: 2rem;
+	color: #a9a9a9;
 `;
 
 export default RecommendItem;
