c43f3632a740793e2d7b6424c74e4e55253fda28
9603100 <imgoing222@gmail.com>
2022-05-18 23:36:27 +0900

    [S06P31D105-225] feat: 내 매물 탭 구현

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -3,13 +3,24 @@ import Favorites from "@components/profile/Bookmarks";
 import Tabs from "@components/profile/Tabs";
 import UserInfo from "@components/profile/UserInfo";
 import { useState } from "react";
+import MyArticle from "@components/profile/MyArticle";
+import Bookmarks from "@components/profile/Bookmarks";
 
 function Profile() {
-	const [onUserInfo, setOnUserInfo] = useState(true);
+	const [currentTab, setCurrentTab] = useState(0);
+
+	const tabs = ["내 정보", "북마크", "내 매물"];
+
+	const getCurrentTab = () => {
+		if (currentTab === 0) return <UserInfo />;
+		else if (currentTab === 1) return <Bookmarks />;
+		else if (currentTab === 2) return <MyArticle />;
+	};
+
 	return (
 		<Container>
-			<Tabs onUserInfo={onUserInfo} setOnUserInfo={setOnUserInfo} />
-			{onUserInfo ? <UserInfo /> : <Favorites />}
+			<Tabs setCurrentTab={setCurrentTab} tabs={tabs} />
+			{getCurrentTab()}
 		</Container>
 	);
 }
