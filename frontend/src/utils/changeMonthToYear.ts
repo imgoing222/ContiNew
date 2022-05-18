ce0ade6004e424c8624b0dbee0d2b01b16589347
우동진 <wjinh93@gmail.com>
2022-05-18 10:08:49 +0900

    [S06P31D105-29] feat: 날짜 함수 수정

--- frontend/src/utils/changeMonthToYear.ts
+++ frontend/src/utils/changeMonthToYear.ts
@@ -1,6 +1,6 @@
 const changeMonthToYear = (month: string) => {
 	if (month) {
-		if (+month > 12)
+		if (+month >= 12)
 			return `${Math.floor(+month / 12)}년  ${+month % 12 > 0 ? (+month % 12) + "개월" : ""}`;
 		return `${month}개월`;
 	}
