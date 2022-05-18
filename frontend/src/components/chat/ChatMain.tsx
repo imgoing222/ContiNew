ca07b52a45846c47ec36c6f99ebdd539df2ffcf7
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-18 11:49:03 +0900

    [S06P31D105-166] style: Content 스타일 추가

--- frontend/src/components/chat/ChatMain.tsx
+++ frontend/src/components/chat/ChatMain.tsx
@@ -1,12 +1,14 @@
+import { faComments } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 function ChatMain() {
 	return (
 		<Container>
 			<Content>
-				<div>메시지 아이콘</div>
-				<h2>내 메시지</h2>
-				<p>메시지를 통해 부족한 정보를 확인하세요.</p>
+				<FontAwesome icon={faComments} />
+				<Title>내 메시지</Title>
+				<Message>메시지를 통해 부족한 정보를 확인하세요.</Message>
 			</Content>
 		</Container>
 	);
@@ -15,15 +17,30 @@ function ChatMain() {
 const Container = styled.div`
 	width: 100rem;
 	height: 100%;
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 `;
 
 const Content = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-  align-items: center;
+	align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+`;
+
+const Message = styled.p`
+  font-size: 2rem;
+`;
+
+const FontAwesome = styled(FontAwesomeIcon)`
+	width: 20rem;
+	height: 20rem;
+  // color: #dc143c;
+	margin-right: 1.5rem;
 `;
 
 export default ChatMain;
