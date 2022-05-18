7588cb1a97e5d8a9a692e5dd95411a74841eca19
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-05-18 11:10:23 +0900

    [S06P31D105-166] feat: ChatMain 초안

--- frontend/src/components/chat/ChatMain.tsx
+++ frontend/src/components/chat/ChatMain.tsx
@@ -1,12 +1,29 @@
 import styled from "styled-components";
 
 function ChatMain() {
-	return <Container>ㅎㅇ</Container>;
+	return (
+		<Container>
+			<Content>
+				<div>메시지 아이콘</div>
+				<h2>내 메시지</h2>
+				<p>메시지를 통해 부족한 정보를 확인하세요.</p>
+			</Content>
+		</Container>
+	);
 }
 
 const Container = styled.div`
 	width: 100rem;
 	height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+  align-items: center;
 `;
 
 export default ChatMain;
