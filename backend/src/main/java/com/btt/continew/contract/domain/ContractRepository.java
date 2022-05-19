8f9e15c7c02c5a6e739398de7fccc45683ecdfd3
IcedCaffeLatte <tmdrl0160@naver.com>
2022-05-20 00:00:48 +0900

    [S06P31D105-58] feat: 계약요청, 계약서 다중 삭제 쿼리 추가

--- backend/src/main/java/com/btt/continew/contract/domain/ContractRepository.java
+++ backend/src/main/java/com/btt/continew/contract/domain/ContractRepository.java
@@ -2,13 +2,23 @@ package com.btt.continew.contract.domain;
 
 import com.btt.continew.house.domain.House;
 import com.btt.continew.member.domain.Member;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     Optional<Contract> findByHouseAndSellerAndBuyer(House house, Member seller, Member buyer);
 
     List<Contract> findAllBySellerOrBuyer(Member seller, Member buyer);
+
+    @Modifying
+    @Query(value = "update Contract c "
+        + "set c.deletedAt = :now "
+        + "where c.house = :house_id")
+    Integer updateDeletedAtByHouse(@Param("house_id") House house, @Param("now") LocalDateTime now);
 }
