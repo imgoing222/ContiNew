7631e327f674fbdc9a10c214d141c18d140cbfdb
9603100 <imgoing222@gmail.com>
2022-05-20 11:53:40 +0900

    [S06P31D105-232] feat: 비로그인 사용자 내 계약 접근 차단

--- frontend/src/pages/contract/index.tsx
+++ frontend/src/pages/contract/index.tsx
@@ -4,15 +4,21 @@ import { contractApi } from "src/api";
 import { MyContracts } from "src/types/MyContracts";
 import Tabs from "@components/profile/Tabs";
 import styled from "styled-components";
+import cookie from "react-cookies";
+import { useRouter } from "next/router";
 
 function MyContractsList() {
+	const accessToken = cookie.load("access_token");
+	const router = useRouter();
+
 	const [onGoingContracts, setOnGoingContracts] = useState<MyContracts[]>([]);
 	const [completedContracts, setCompletedContracts] = useState<MyContracts[]>([]);
 	const [currentTab, setCurrentTab] = useState(0);
 
 	const tabs = ["계약중", "계약완료"];
+
 	useEffect(() => {
-		getMyContracts();
+		if (accessToken) getMyContracts();
 	}, []);
 
 	const getMyContracts = async () => {
@@ -24,7 +30,10 @@ function MyContractsList() {
 		});
 	};
 
-	return (
+	const goToSignIn = () => {
+		router.push("/account/signin");
+	};
+	return accessToken ? (
 		<Container>
 			<Tabs setCurrentTab={setCurrentTab} tabs={tabs} />
 			<MyContractsForm
@@ -32,6 +41,8 @@ function MyContractsList() {
 				currentTab={currentTab}
 			/>
 		</Container>
+	) : (
+		goToSignIn()
 	);
 }
 
