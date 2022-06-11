ac5d2a66104bed278f9b1192f5e1baae1f24caeb
9603100 <imgoing222@gmail.com>
2022-06-12 06:52:37 +0900

    [S06P31D105-236] refactor: import 정리 및 변수 리팩토링

---
+++ frontend/src/components/account/Index.ts
@@ -0,0 +1,5 @@
+export { Button as Button } from "./Button";
+export { Container as Container } from "./Container";
+export { FormContainer as FormContainer } from "./Container";
+export { Header as Header } from "./Header";
+export { Input as Input } from "./Input";
